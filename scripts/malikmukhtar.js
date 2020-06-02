7c4a5217d7a603dd855855d13c02d6413beb9562
Malik <malikmukhtar63@gmail.com>
2020-06-03 00:49:09 +0100

    added my script

---
+++ scripts/malikmukhtar.js
@@ -0,0 +1,7 @@
+const newTask = (name, id) => {
+  let output = `Hello World, this is ${name} with HNGi7 ID: ${id} using Javascript for stage 2 task`;
+
+  return console.log(output);
+};
+
+newTask("Malik Mukhtar", "HNG-03048");
