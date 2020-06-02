37d4d081e75e6c4e3a70255df1e2637add4f7760
Noble-47 <noblebest56@gmail.com>
2020-06-03 00:51:48 +0100

    Update noble.py

--- scripts/noble.py
+++ scripts/noble.py
@@ -2,4 +2,9 @@ full_name = 'Onunwa Goodness'
 id = 'HNG-05776'
 language = 'python'
 
-print(f'Hello World, this is {full_name} with HNGi7 ID {id} using {language} for stage 2 task')
+statement = f'Hello World, this is {full_name} with HNGi7 ID {id} using {language} for stage 2 task'
+
+def print_statement(statement):
+    return statement
+
+print_statement(statement)
