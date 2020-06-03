405954f403c5d4475491a12689c774f4826f7689
Amanie-hue <amaniesandy355@gmail.com>
2020-06-03 13:39:11 +0100

    first commit

---
+++ scripts/HNG-00307.js
@@ -0,0 +1,15 @@
+
+const message = 'Hello World' ;
+const name = 'Amanam Enobong Sandy';
+const HNGi7_ID = 'HNG-00307';
+const language = 'javascript';
+const email = 'amaniesandy355@gmail.com'
+
+const User = {
+    message:'message',
+    name: 'name',
+    HNGi7_ID: 'HNG',
+    language: 'JS'
+}
+console.log(JSON.stringify(message + ' this is '+ name + ' with HNGI7 ID :'+ HNGi7_ID + ' using ' + language + ' for stage 2 task'))
+
