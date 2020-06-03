a8abc9c06b6c1af35757fa6ecbecc259bfbd038d
Technelly <61063626+Technelly@users.noreply.github.com>
2020-06-03 23:01:18 +0100

    My js commit
    My javascript file for team task

--- scripts/technelly.js
+++ scripts/technelly.js
@@ -5,8 +5,6 @@ var myInfo = {
   id     : "HNG-02677",
   myEmail: "greatabah1@gmail.com",
   myLang:   "Javascript",
-
-
 };
  function myDetails() {
     return "Hello world, this is" + " " + myInfo.myName + " " + "with ID" + " " + myInfo.id + " " + "and Email:" + " " + myInfo.myEmail + " " + "using" + " " + myInfo.myLang + " " + "for stage 2.";
