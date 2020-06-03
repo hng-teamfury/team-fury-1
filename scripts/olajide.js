cbfda5bea9632ad6c8146230cf00b04d3842d387
oelun-In <55861946+oelun-in@users.noreply.github.com>
2020-06-03 04:10:46 +0100

    Delete olajide.js

--- scripts/olajide.js
+++
@@ -1,7 +0,0 @@
-function User () {
-  const mydetails = {fullname: 'olajide oluniyi', ID: 'HNG-01994', language: 'javascript'}
-  const myDetails = 'Hello World, this is ' + mydetails.fullname + ' with HNGi7 ID ' + mydetails.ID + ' using ' + mydetails.language + ' for stage 2 task' ;
-  const intern = JSON.stringify(myDetails)
-  return intern}
-
-User()
