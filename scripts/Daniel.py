5978c10847a170675c8364868d04bec0736fb744
dan-ibeh <65494402+dan-ibeh@users.noreply.github.com>
2020-06-03 10:05:43 +0100

    Add files via upload

---
+++ scripts/Daniel.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  3 09:19:01 2020
+
+@author: Daniel
+"""
+
+
+name = "Daniel Ibeh"
+ID = "HNG-01541"
+email = "dibeh1000@gmail.com"
+language = "Python"
+status = "Pass"
+print("Hello World, this is", name, "with HNGi7 ID", ID, "using", language, "for stage 2 task")
