2ca6a00f72cf758c98a8d650f2c73b0d9ee0bba4
feyifoluwa <abioyefeyifoluwa@gmail.com>
2020-06-03 10:53:58 +0100

    add obiwan script

---
+++ scripts/obiwan.js
@@ -0,0 +1,6 @@
+let name = "Abioye Feyifoluwa";
+let id = "HNG-00889";
+let language = "JavaScript";
+let email = "abioyefeyifoluwa@gmail.com"
+
+console.log(`Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task`)
