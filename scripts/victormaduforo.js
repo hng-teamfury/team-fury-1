0027003fac8d41fc4751291b14d94edcaebac5b9
Victor Maduforo <victormaduforo@gmail.com>
2020-06-03 18:43:46 +0100

    added victor maduforo task 2 script

---
+++ scripts/victormaduforo.js
@@ -0,0 +1,6 @@
+var details={
+    fullName: "Victor Maduforo",
+    ID: "HNG-03307",
+    language: "Javascript"
+};
+console.log("Hello World, this is %s with HNGi7 ID %s using %s for stage 2 task", details.fullName, details.ID, details.language);
