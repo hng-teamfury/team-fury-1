4c35817b608b7ca96f872968715234b12fa57c77
Sorin-tech <66221497+Sorin-tech@users.noreply.github.com>
2020-06-03 18:34:44 +0100

    New Python notebook added
    my script for stage 2 task

---
+++ Sorin Script.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+def myscript():
+    name = 'Sorinola Michael'
+    ID = 'HNGi7 02213'
+    language = 'python'
+    print(f'Hello World, this is {name} with id {ID} using {language} for stage 2 task.')
+
+
+# In[6]:
+
+
+myscript()
+
+
+# 
+
+# In[ ]:
+
+
+
+
