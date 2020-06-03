1450fbb246cae8f2d75d055e2b6b7b7ab2e87ce6
Nyior Clement Jr <nyior.clement@aun.edu.ng>
2020-06-03 17:06:03 +0100

    added nyior's js script

---
+++ scripts/nyior.js
@@ -0,0 +1,15 @@
+// user data
+
+const interName = "Nyior Clement"
+const internId = "HNG-05586"
+const programmingLanguage = "JavaScript"
+
+//function that returns the required text output
+
+function printUserData(name, id, language) {
+
+	return `Hello World, this is ${name} with HNGi7 ID  ${id} using ${language} for stage 2`
+	
+}
+
+printUserData(interName, internId, programmingLanguage)
