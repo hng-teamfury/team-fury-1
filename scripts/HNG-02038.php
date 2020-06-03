304041f1c407fb6fe46c97922652f0c7e034fc55
itorophilip1998 <itkeyzzpianny@gmail.com>
2020-06-03 15:41:35 +0100

    initial commit

---
+++ scripts/HNG-02038.php
@@ -0,0 +1,11 @@
+<?php
+    
+        $fullname = "Imeobong Edem Akpan";
+        $email = "imeakpan5050@gmail.com";
+        $hngid = "HNG-02038";
+        $language = "PHP";
+
+    echo "Hello World this is {$fullname} with HNGi7 ID, {$hngid} using {$language} for stage 2 task";
+?>
+
+
