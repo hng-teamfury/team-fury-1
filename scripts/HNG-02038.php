d884e76285b4f0bd092ffc0f4db9cf57683965a7
itorophilip1998 <itkeyzzpianny@gmail.com>
2020-06-03 16:01:30 +0100

    new commit

--- scripts/HNG-02038.php
+++ scripts/HNG-02038.php
@@ -5,7 +5,7 @@
         $hngid = "HNG-02038";
         $language = "PHP";
 
-    echo "Hello World this is {$fullname} with HNGi7 ID, {$hngid} using {$language} for stage 2 task";
+    echo "Hello World this is {$fullname} with HNGi7 ID {$hngid} using {$language} for stage 2 task";
 ?>
 
 
