7d2fdd51de0a700a1158228e7ead853f23ce2ce8
Nyior Clement Jr <nyior.clement@aun.edu.ng>
2020-06-03 19:17:16 +0100

    modified my js script

--- scripts/nyior.js
+++ scripts/nyior.js
@@ -3,13 +3,14 @@
 const interName = "Nyior Clement"
 const internId = "HNG-05586"
 const programmingLanguage = "JavaScript"
+const iternEmail = "cnyior27@gmail.com"
 
 //function that returns the required text output
 
-function printUserData(name, id, language) {
+function printUserData(name, id, language, email) {
 
-	return `Hello World, this is ${name} with HNGi7 ID  ${id} using ${language} for stage 2`
+	return `Hello World, this is ${name} with HNGi7 ID  ${id} and Email ${email}using ${language} for stage 2`
 	
 }
 
-printUserData(interName, internId, programmingLanguage)
+printUserData(interName, internId, programmingLanguage, iternEmail)
