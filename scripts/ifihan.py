5e5bac594ba85317502d75a7d316b4707063749e
Ifihanagbara Olusheye <victoriaolusheye@gmail.com>
2020-06-03 03:34:47 +0100

    Added my Python Script

---
+++ scripts/ifihan.py
@@ -0,0 +1,8 @@
+name = 'Ifihanagbara Olusheye'
+id = 'HNG-04984'
+language = 'Python'
+
+def user():
+	print (f'Hello World! My name is {name} with HNGi7 ID: {id} and this script is written in {language}')
+
+user()
