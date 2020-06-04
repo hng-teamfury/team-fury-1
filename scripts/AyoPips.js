5cc130f5c8ae2c6fd71972206bb5b2ed896ab561
Ayobami Faneye <62366768+AyoPips@users.noreply.github.com>
2020-06-04 23:23:27 +0800

    Update AyoPips.js

--- scripts/AyoPips.js
+++ scripts/AyoPips.js
@@ -2,5 +2,5 @@ let fullName = "Ayobami Faneye";
 let id = "HNG-00934";
 let Language = "Javascript";
 let email = "engineerfao@gmail.com";
-let output = "Hello World, this is " + fullName + " with HNGi7  " + id + " using " + Language + " for stage 2 task.";
+let output = "Hello World, this is " + fullName + " with HNGi7  " + id + " and email " +email+" using" + Language + " for stage 2 task.";
 console.log(output);
