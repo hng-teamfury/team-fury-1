5e030ee18b56f71e83704ec7300446c5a046e6e9
fridaychukwuamaka <fridaychukwuamaka@gmail.com>
2020-06-04 17:18:05 +0100

    Friday Chukwuamaka's script

--- scripts/friday_chukwuamaka.js
+++ scripts/friday_chukwuamaka.js
@@ -3,6 +3,6 @@ var fullName = "Friday Chukwuamaka",
   language = "javaScript",
   myEmail = "fridaychukwuamaka@gmail.com";
 
-var result = `Hello world, this is ${fullName} with HNGi7 ID ${myId} and email ${myEmail} using ${language} for stage 2 task`;
+var result = `Hello World, this is ${fullName} with HNGi7 ID ${myId} and email ${myEmail} using ${language} for stage 2 task`;
 
 console.log(result);
