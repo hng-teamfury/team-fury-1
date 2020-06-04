5e73d2bdb1e127962efa7878efa0019b74baf0b5
Cyril <41521163+ThatSameBlackBoyFromAfrica@users.noreply.github.com>
2020-06-05 00:55:41 +0100

    Add files via upload
    HNGi7 team task

---
+++ scripts/CyrilScript.java
@@ -0,0 +1,10 @@
+package com.chukwuebuka;
+
+public class CyrilScript {
+    public static void main(String[] args) {
+        String name = "Chukwuebuka Muofunanya";
+        String hngId = "HNG-00745";
+        String language = "Java";
+        System.out.println("Hello World, this is " + name + " with HNGi7 ID " + hngId + " using " + language + " for stage 2 task");
+    }
+}
