9a8564d9ac9a36b9a157a5ebd51584978daff672
Damilare50 <damilarelamine@gmail.com>
2020-06-04 13:34:55 +0100

    Damilare2020's JavaScript

---
+++ scripts/Damilare2020.js
@@ -0,0 +1,8 @@
+var data = {
+    fullName:"Oluwadamilare Okubanjo",
+    id:"HNG-02526",
+    email:"Damilarelamine@gmail.com",
+    language:"JavaScript"
+};
+var x = "Hello World, this is " + data.fullName + " with HNGi7 ID " + data.id + " using " + data.language + " for stage 2 task";
+console.log(x);
