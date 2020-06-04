c147c6c31bf3a7296c05bc2c0c2823893bf77404
Damilare50 <66230820+Damilare50@users.noreply.github.com>
2020-06-04 13:52:12 +0100

    Delete Damilare2020.js

--- scripts/Damilare2020.js
+++
@@ -1,8 +0,0 @@
-var data = {
-    fullName:"Oluwadamilare Okubanjo",
-    id:"HNG-02526",
-    email:"Damilarelamine@gmail.com",
-    language:"JavaScript"
-};
-var x = "Hello World, this is " + data.fullName + " with HNGi7 ID " + data.id + " and email " + data.email + " using " + data.language + " for stage 2 task";
-console.log(x);
