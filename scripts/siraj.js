0b7f6979f2a298b4b8814f7c347f57646578c95e
surahj <odebodeopeyemi@gmail.com>
2020-06-04 04:38:46 +0100

    add my script

---
+++ scripts/siraj.js
@@ -0,0 +1,13 @@
+const dataObj ={
+    name : "Odebode Surajudeen Opeyemi",
+    id:   2731,
+    language: "javascript",
+    email: "odebodeopeyemi@gmail.com"
+};
+
+
+const {name, id, language, email} = dataObj;
+
+const data = `Hello World,this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task `;
+
+console.log(data);
