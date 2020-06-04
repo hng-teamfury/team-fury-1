5fe04dcd59c25a349b053341b3fec17f1f0da1a1
fridaychukwuamaka <fridaychukwuamaka@gmail.com>
2020-06-04 17:24:29 +0100

    Friday Chukwuamaka's script

--- scripts/friday_chukwuamaka.js
+++ scripts/friday_chukwuamaka.js
@@ -1,6 +1,6 @@
 var fullName = "Friday Chukwuamaka",
   myId = "HNG-00768",
-  language = "javaScript",
+  language = "JavaScript",
   myEmail = "fridaychukwuamaka@gmail.com";
 
 var result = `Hello World, this is ${fullName} with HNGi7 ID ${myId} and email ${myEmail} using ${language} for stage 2 task`;
