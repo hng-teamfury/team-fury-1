0d276f80ed080ac7f6e5542e760d3f62033a620e
Noble-47 <noblebest56@gmail.com>
2020-06-04 18:14:02 +0100

    updated noble.py
    added my email to output

--- scripts/noble.py
+++ scripts/noble.py
@@ -2,8 +2,9 @@
 full_name = 'Onunwa Goodness'
 id = 'HNG-05776'
 language = 'python'
+email = 'Noblebest56@gmail.com'
 
-statement = f'Hello World, this is {full_name} with HNGi7 ID {id} using {language} for stage 2 task'
+statement = f'Hello World, this is {full_name} with HNGi7 ID {id} and email {email} using {language} for stage 2 task'
 
 def print_statement(statement):
     return statement
