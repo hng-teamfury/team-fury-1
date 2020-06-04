a92e8888cc88f4fe190dd6efd410855ef96ba66a
SOLOMON-PC\Solomon <solomon.iroegbu@gmail.com>
2020-06-04 21:30:21 +0100

    add my solo-p.php script

--- scripts/solo-p.php
+++ scripts/solo-p.php
@@ -1,11 +1,11 @@
 <?php
 
-	$fullname = 'Solomon Iroegbu';
+	$fullName = 'Solomon Iroegbu';
 	$id = 'HNG-05822';
 	$language = 'PHP';
 	$email = 'solomon.iroegbu@gmail.com';
 
-	echo  "Hello World, this is $fullName with HNGi7 ID $id and email $email using $language for stage 2 task";
+	echo  "Hello World, this is {$fullName} with HNGi7 ID {$id} and email {$email} using {$language} for stage 2 task";
 
 ?>
 
