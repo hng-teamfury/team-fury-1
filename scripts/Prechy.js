e52d6659dd4ca911576684ac958706b48d3dbeae
Prechy1211 <66218597+Prechy1211@users.noreply.github.com>
2020-06-05 00:21:10 +0100

    uodate prechy.js
    updated this

--- scripts/Prechy.js
+++ scripts/Prechy.js
@@ -1,6 +1,12 @@
-let fullName = "Precious Okafor";
-let id = "HGN-00208";
-let lang = "Javascript";
-let email = "preciousrobinsonokafor@gmail.com";
-//output should look like this: Hello World, this is Precious Okafor with HGN ID: HGN-00208 using Javascript fot stage two task 
-console.log(`Hello World, this is ${fullName} with HGN ID ${id} using ${lang} for stage 2 task`);
+function hello() {
+  let info = {
+    fullname: "Precious Okafor",
+    ID: "HNG-00208",
+    language: "Javascript",
+    email: "preciousrobinsonokafor@gmail.com"
+  }
+
+  return `Hello World, this is ${info.fullname} with HNGi7 ID ${info.ID} and email ${info.email} using ${info.language} for stage 2 task`;
+}
+
+console.log(hello());
