eff2215052459ea52ce672a613eb239cb6fc5437
Joel Aliyu <joelaliyu1@gmail.com>
2020-06-04 01:42:30 +0100

    Updating the hng-00779.js file

--- scripts/hng-00779.js
+++ scripts/hng-00779.js
@@ -6,7 +6,7 @@ function Information() {
       email: "joelaliyu1@gmail.com"
     }
     
-    return `Hello World, this is ${details.fullname} with HNGi7 ID ${details.ID} using ${details.language} for stage 2 task`;
+    return `Hello World, this is ${details.fullname} with HNGi7 ID ${details.ID} and email ${details.email} using ${details.language} for stage 2 task`;
 }
 
   Information();
