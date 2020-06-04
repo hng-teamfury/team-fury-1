f159f94a1ec30a8643f6730d062fe3ab77b4f49d
kelechinathan97@gmail.com <kelechinathan04@gmail.com>
2020-06-04 05:37:36 +0100

    This is my script

---
+++ HNG-00363.js
@@ -0,0 +1,7 @@
+let person = {
+  name: "Attah Kelechi Nathan",
+  ID: "HNG-00363",
+  Language: "Javascript"
+};
+let Task2_Message = "Hello World, this is" + " " + person.name + " " + "with HNGi7 ID" + " " + person.ID + " " + "using" + " " + person.Language + " " + "for stage 2 task.";
+console.log(Task2_Message);
