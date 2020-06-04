7b074ba10423da3c81ae29ee7794d3fd02b66f3e
Francisop <francisohis@gmail.com>
2020-06-04 18:56:52 +0100

    changed language

--- scripts/francis.py
+++ scripts/francis.py
@@ -2,8 +2,9 @@ import json
 
 first_name="Francis Onwukwe"
 hngi7_id = "03944"
+email ="francisohis@gmail.com"
 language = "Python"
 
-data = "Hello World,this is {} with  HNGi7 ID HNG-{} and language{} for stage 2 task".format(first_name,hngi7_id,language)
+data = "Hello World,this is {} with  HNGi7 ID HNG-{} and email {} using {} for stage 2 task".format(first_name,hngi7_id,email,language)
 json_output = json.dumps(data,indent=1)
 print(json_output)
