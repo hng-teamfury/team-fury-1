e4e85ed52aed0453bd0cc16b7525a1850adccdab
Mbah Stanley <stanmest77@gmail.com>
2020-06-04 19:47:39 +0100

    mest script

---
+++ scripts/mest.py
@@ -0,0 +1,12 @@
+# Declared global variables
+name = "STANLEY MBAH"
+id = "HNG-04589"
+email = "stanmest77@gmail.com"
+language = "Python"
+
+
+def my_script(name, id, email, language):
+    print("Hello World, this is  {}  with HNGi7 ID {} and email {} using {} for stage 2 task".format(name,id,email,language))
+
+    
+my_script(name, id, email, language)
