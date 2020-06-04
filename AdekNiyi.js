fd911e453c01aaff2129e3206067165b13178c44
Adek Niyi <adekniyi@gmail.com>
2020-06-04 13:12:37 -0700

    my first task

---
+++ AdekNiyi.js
@@ -0,0 +1,11 @@
+var myData = {
+    firstName: "Adekunle",
+    lastName:"Adeniyi",
+    email:"Adekniyi@gmail.com",
+    language:"HTML, CSS, JavaScript",
+    hng_id:"HNG-03886"
+}
+console.log(myData.firstName)
+var output = `Hello world this is ${myData.firstName} ${myData.lastName} with HNGi7 ID ${myData.hng_id} using ${myData.language} for stage 2 task`                                                                                                                                                       
+
+console.log(output)
