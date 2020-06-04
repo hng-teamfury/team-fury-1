fd9d5a9d765af8edbb1bdd255aed70f574702d97
alimisulaimon1 <52748665+alimisulaimon1@users.noreply.github.com>
2020-06-04 18:02:52 -0400

    add my script

---
+++ scripts/sular007.php
@@ -0,0 +1,11 @@
+
+	<?php 
+	$fullName = "Alimi Sulaimon";
+	$email = "alimisulaimon1@gmail.com";
+	$hngId = "HNG-01106";
+	$languageUsed = "PHP";
+	$stage = "stage 2";
+
+	echo "Hello World, this is {$fullName} with HNGi7 ID {$hngId} and email {$email} using {$languageUsed} for {$stage} task";
+
+	?>
