211a98daae6358d055a16cb2579de9b7cc1e7320
Babalola Samson <smartbabs02@gmail.com>
2020-06-04 22:39:44 +0100

    Deleted unnecessary file

--- scripts/Samson_Babalola_old.py
+++
@@ -1,5 +0,0 @@
-full_name = 'Samson Babalola'
-email:'smartbabs02@gmail.com'
-hngi7_id = 'HNG-06555'
-language = 'Python'
-print(' Hello World! This is {} with HNGi7 ID: {} using {}'. format(full_name,hngi7_id, language))
