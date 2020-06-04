cc0847a0693f974fc591e3bac19cbc2c18cde6fe
bv.adegboye <64808662+bvadegboye@users.noreply.github.com>
2020-06-04 21:38:49 +0100

    Create Victoria.js

---
+++ scripts/Victoria.js
@@ -0,0 +1,6 @@
+let fullName = "Victoria Adegboye";
+let id = "HNG-00157";
+let language = "JavaScript";
+let email = "bv.adegboye@gmail.com";
+let output = `Hello World, this is ${fullName} with HNGi7 id ${id} using ${language} for stage 2 task`;
+console.log(output);
