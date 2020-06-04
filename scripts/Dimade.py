d1a3d290a09419fe4422d77a20f96636824c0828
Crestward <65926941+Crestward@users.noreply.github.com>
2020-06-04 18:11:14 +0100

    Added Python file

---
+++ scripts/Dimade.py
@@ -0,0 +1,8 @@
+fullname = "Oladimeji Olamide Adeyemi"
+ID = "HNG-02518"
+email = "oladimeji759@gmail.com"
+language = "Python"
+
+output = f"Hello world, this is {fullname} with HNDi7 ID {ID} and email {email} using {language} for stage 2 task."
+
+print(output)
