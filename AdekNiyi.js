6962bf9e22f9941a0e97a5a1ee0d0c3dc424f351
Adek Niyi <adekniyi@gmail.com>
2020-06-04 14:28:55 -0700

    my first task

--- AdekNiyi.js
+++ AdekNiyi.js
@@ -1,11 +1,21 @@
-var myData = {
-    firstName: "Adekunle",
-    lastName:"Adeniyi",
-    email:"Adekniyi@gmail.com",
-    language:"HTML, CSS, JavaScript",
-    hng_id:"HNG-03886"
-}
-console.log(myData.firstName)
-var output = `Hello world this is ${myData.firstName} ${myData.lastName} with HNGi7 ID ${myData.hng_id} using ${myData.language} for stage 2 task`                                                                                                                                                       
+function myfunction() {
+    var myData = {
+        firstName: "Adekunle",
+        lastName:"Adeniyi",
+        email:"Adekniyi@gmail.com",
+        language:"HTML, CSS, JavaScript",
+        hng_id:"HNG-03886"
+    }
+    return `Hello World, this is ${myData.firstName} ${myData.lastName} with HNGi7 ID ${myData.hng_id}  using ${myData.language} for stage 2 task`                                                                                                                                                       
 
-console.log(output)
+}
+
+
+var data = myfunction()
+
+console.log(data)
+
+
+
+
+
