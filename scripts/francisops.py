286ffbd1e4868959fd44a48f095f57b3843a815b
Francisop <francisohis@gmail.com>
2020-06-04 18:32:00 +0100

    corrected my script

--- scripts/francisops.py
+++ scripts/francisops.py
@@ -2,8 +2,8 @@ import json
 
 first_name="Francis Onwukwe"
 hngi7_id = "03944"
-lang = "Python"
+language = "Python"
 
-data = "Hello World,this is {} with  HNGi7 ID HNG-{} and Im using {} for stage 2 task".format(first_name,hngi7_id,lang)
+data = "Hello World,this is {} with  HNGi7 ID HNG-{} and email {} for stage 2 task".format(first_name,hngi7_id,language)
 json_output = json.dumps(data,indent=1)
 print(json_output)
