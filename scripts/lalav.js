304984540f9f7425759813236b781b6c3f43d69c
varlov <51709867+varlov@users.noreply.github.com>
2020-06-04 23:32:25 +0100

    Please, use

--- scripts/lalav.js
+++ scripts/lalav.js
@@ -1,3 +1,5 @@
-let name = "Rebecca Lalav", id =  HNG-00993", email = "dovelalove@gmail.com", language = "Javascript";
+function hng(name, id, language, email){
+    console.log(`Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`)
+}
 
-console.log("Hello World, I am " + name + " with HNGi7 ID " + id + " and email " + email + " using " + language + " for stage 2 task")
+hng("Rebecca Lalav", "HNG-00993", "JavaScript", "dovelalove@gmail.com" )
