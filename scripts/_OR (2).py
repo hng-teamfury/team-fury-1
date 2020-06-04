d45212ab15834d75f5b15e647a2f059181422037
fir3fly89 <42363666+fir3fly89@users.noreply.github.com>
2020-06-05 00:36:58 +0100

    Kindly add my Script

--- scripts/_OR (2).py
+++
@@ -1,9 +0,0 @@
-def startng():
-    name = 'Onyewotu Ralph'
-    hngid = 'HNG-04445'
-    email = 'ralphoc89@gmail.com'
-    language = 'PYTHON'
-    print(f'Hello World, this is {name}, with HNGi7 ID: {hngid} using {language} for Stage 2 task.')
-
-startng()
-
