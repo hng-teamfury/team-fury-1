ae9e1695d3e3a4472477e9d8b36de49a1eed2649
DerKaiser7 <44611064+DerKaiser7@users.noreply.github.com>
2020-06-04 12:57:05 +0100

    updated script

--- scripts/derkaiser-HNG-02844.php
+++ scripts/derkaiser-HNG-02844.php
@@ -1,7 +1,7 @@
 <?php
     $info = array("fullname"=>"Falodu Stephen Abimbola", "hng_id"=>"HNG-02844", "language"=>"php", "email"=>"stephenfalodu@icloud.com");
 
-    $result = "Hello World, this is " . $info['fullname'] . " with HNGi7 ID: " . $info['hng_id'] . " and email " . $info['email'] . " using " . $info['language'] . " for stage 2 task";
+    $result = "Hello World, this is " . $info['fullname'] . " with HNGi7 ID " . $info['hng_id'] . " and email " . $info['email'] . " using " . $info['language'] . " for stage 2 task";
 
     echo $result;
 
