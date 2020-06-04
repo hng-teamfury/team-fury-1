3eabf4f32dd33b072dbc29273413a3966a9e201b
Marvellous1 <51808658+Marvellous1@users.noreply.github.com>
2020-06-04 18:10:30 +0100

    Little edit

--- scripts/Marvellous.js
+++ scripts/Marvellous.js
@@ -5,7 +5,7 @@ let My_Language = "javascript";
 let My_Email = "aigbemarvellous01@gmail.com";
 
 //My message
-const My_Message = `Hello world, this is ${My_Name} with HNGi7 ID ${My_ID} and email ${My_Email} using ${My_Language} for stage 2 task`;
+const My_Message = `Hello World, this is ${My_Name} with HNGi7 ID ${My_ID} and email ${My_Email} using ${My_Language} for stage 2 task`;
 
 //Sending Message
 console.log(My_Message);
