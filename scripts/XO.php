04e1474b60e2b43411279757aca8a12419f3d1e4
Phoxtrot <yinkarhy@gmail.com>
2020-06-04 17:01:10 +0100

    My script is saved as XO

---
+++ scripts/XO.php
@@ -0,0 +1,9 @@
+<?php
+ $name = 'Yusuf Muhammad Toyib';
+ $HNG_ID = 'HNG-01908';
+ $email = 'yinkarhy@gmail.com';
+ $language = 'php';
+
+ echo 'Hello world, this is '.$name.
+	' with HNGi7 ID '.$HNG_ID.' and email '.$email.' using '.$language.' for stage 2 task';
+
