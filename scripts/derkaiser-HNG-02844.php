9bc5947dfed3f8bae9200d352888b883022423ca
DerKaiser7 <44611064+DerKaiser7@users.noreply.github.com>
2020-06-04 10:32:34 +0100

    added a variable for result

--- scripts/derkaiser-HNG-02844.php
+++ scripts/derkaiser-HNG-02844.php
@@ -1,8 +1,9 @@
 <?php
     $info = array("fullname"=>"Falodu Stephen Abimbola", "hng_id"=>"HNG-02844", "language"=>"php", "email"=>"stephenfalodu@icloud.com");
 
-    echo "Hello World, this is " . $info['fullname'] . " with HNGi7 ID: " . $info['hng_id'] . " using " . $info['language'] . " for stage 2 task";
+    $result = "Hello World, this is " . $info['fullname'] . " with HNGi7 ID: " . $info['hng_id'] . " using " . $info['language'] . " for stage 2 task";
 
+    echo $result;
     echo json_encode ($info);
 
 ?>
