74e39dceec34a2f817a466cb71ef6f2ad9afdaa3
Prechy1211 <66218597+Prechy1211@users.noreply.github.com>
2020-06-04 22:29:30 +0100

    #Precious added her script

---
+++ scripts/Precious.js
@@ -0,0 +1,6 @@
+let fullName = "Precious Okafor";
+let id = "HGN-00208";
+let lang = "Javascript";
+let email = "preciousrobinsonokafor@gmail.com";
+//output should look like this: Hello World, this is Precious Okafor with HGN ID: HGN-00208 using Javascript fot stage two task 
+console.log(`Hello World, this is ${fullName} with HGN ID ${id} and email ${email} using ${lang} for stage 2 task`);
