a2ed72779f408dcefacdd15d36ca471b2740f41a
Tolucde <61968366+Tolucde@users.noreply.github.com>
2020-06-04 23:15:11 +0100

    your message

--- scripts/tolu1.js
+++ scripts/tolu1.js
@@ -1,6 +1,11 @@
 const tolu = {
     name: "Tolu Philip",
     email: "toluphilip619@gmail.com",
-    hng: "HNG-04825",
+    hngi7_ID: "HNG-04825",
     language: "javascript",
-};
+    stage: 2
+}
+
+const data = `Hello World, this is ${tolu.name} with HNGi7 ID ${tolu.HNGi7_ID} and email ${tolu.email} using ${tolu.language} for stage ${tolu.stage} task`;
+
+console.log(data);
