d8661132263e2541d28ccc199e901fe7fd32a5a2
Sizo527 <66225554+Sizo527@users.noreply.github.com>
2020-06-04 12:51:47 +0200

    script file

---
+++ scripts/Sizo.py
@@ -0,0 +1,6 @@
+full_name = 'Sibongusizolwenkosi'
+email = 'sizosibanda14@gmail.com'
+hngi7_id = 'HNG-02873'
+language = 'Html and CSS'
+
+print(' Hello World! This is {} with HNGi7 ID: {} using {}'. format(full_name,hngi7_id, language))
