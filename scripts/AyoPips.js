c2232b9a7edfa9d997a0a6308de1140a53a1811f
engineerfao@gmail.com <engineerfao@gmail.com>
2020-06-04 22:58:46 +0800

    Create AyoPips.js

---
+++ scripts/AyoPips.js
@@ -0,0 +1,6 @@
+let fullName = "Ayobami Faneye";
+let id = "HNG-00934";
+let Language = "Javascript";
+let email = "engineerfao@gmail.com";
+let output = "Hello World, this is " + fullName + " with HNGi7  " + id + " using " + Language + " stage 2 task.";
+console.log(output);
