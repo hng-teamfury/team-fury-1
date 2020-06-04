0b6d98f1d4a21e57410c4902c41177a624e151aa
Onuoha Collins <onuohacollinschidubem@gmail.com>
2020-06-03 20:05:31 -0700

    add my own script

---
+++ scripts/collins.js
@@ -0,0 +1,12 @@
+const Detail =() => {
+    const name = "onuoha Collins";
+    const id = "HNG-01854";
+    const email = "onuohacollinschidubem@gmail.com";
+    const language = "javaScript";
+
+    return `Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`
+
+}
+
+
+console.log(Detail());
