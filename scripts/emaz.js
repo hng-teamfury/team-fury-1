457fe2a239cd199e330170b1b535f0b8eab479da
EmmanuelOluwafemi <46839679+EmmanuelOluwafemi@users.noreply.github.com>
2020-06-04 08:33:55 +0100

    add email to my script

--- scripts/emaz.js
+++ scripts/emaz.js
@@ -6,7 +6,7 @@ function speach(){
         language: 'javascript',
         email: 'emaz4me@gmail.com'
     }
-    return console.log(`Hello World, this is ${details.fullname} with HNGi7 ID ${details.id} using ${details.language} for stage two task`)
+    return console.log(`Hello World, this is ${details.fullname} with HNGi7 ID ${details.id} and email ${details.email} using ${details.language} for stage 2 task`)
 }
 
 speach()
