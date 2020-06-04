a1b1747c84af12f38fb65d419ee558c5d0836e0e
TonyOuma <kyrietony@gmail.com>
2020-06-04 16:09:54 +0300

    add script for Ouma to be output

---
+++ scripts/ouma.js
@@ -0,0 +1,10 @@
+var status = "pass";
+var email = "oumat621@gmail.com";
+// function to output the required details
+function showMyDetails() {
+    var id = "HNG-02585";
+    var lang = "JavaScript";
+    var name = "Tony Ouma";
+    return `Hello World, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task`;
+}
+showMyDetails();
