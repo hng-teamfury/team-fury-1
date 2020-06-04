bd7353fb0aa576d830803f9e5f79772e2af374be
Mrkellard <65710801+Mrkellard@users.noreply.github.com>
2020-06-04 23:49:46 +0100

    minor changes was done

--- scripts/mrkellard.js
+++ scripts/mrkellard.js
@@ -1,8 +1,7 @@
-var mrkellard = "Eze Elisha uchechukwu", Hngi7_Id = "HNG-00317", email = "kellard4u@yahoo.com" language= "Javascript";
+var mrkellard = "Eze Elisha uchechukwu", Hngi7_Id = "HNG-00317", email = "kellard4u@yahoo.com", language= "Javascript" ;
 
 function output(){
-    var result= "Hello World, this is" + mrkellard + "with HNGi7 ID" + Hngi7_Id + "and email" + email +
-     "using" + language + "for stage 2 task";
+    var result= `Hello World, this is ${mrkellard} with HNGi7 ID ${Hngi7_Id} and email ${email} using ${language}for stage 2 task`;
      return result;
 }
 
