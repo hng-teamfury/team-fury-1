edd10aabc26e5ffd9a09a34f2bbc920f72181dd0
Thobeats <41216440+Thobeats@users.noreply.github.com>
2020-06-04 16:01:31 +0100

    Update thobeats.php

--- scripts/thobeats.php
+++ scripts/thobeats.php
@@ -1,6 +1,6 @@
 <?php 
 
- echo "Hello World, this is Dayisi Iyanuoluwa Tobi with HNGi7 ID HNG-03757 and email tobiy23@gmail.com, using PHP for stage 2 task";
+ echo "Hello World, this is Dayisi Iyanuoluwa Tobi with HNGi7 ID HNG-03757, using PHP for stage 2 task";
 
 
 
