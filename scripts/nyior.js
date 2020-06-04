20b71d58dd844671e2c29f1134fab54c84169d2b
Nyior Clement Jr <nyior.clement@aun.edu.ng>
2020-06-04 20:49:46 +0100

    removed email from string output

--- scripts/nyior.js
+++ scripts/nyior.js
@@ -1,16 +1,21 @@
 // user data
 
-const interName = "Nyior Clement"
-const internId = "HNG-05586"
-const programmingLanguage = "JavaScript"
-const iternEmail = "cnyior27@gmail.com"
+const userData = {
+    internName: "Nyior Clement",
+    internId: "HNG-05586",
+    programmingLanguage: "Javascript",
+    iternEmail: "cnyior27@gmail.com",
+}
+
 
 //function that returns the required text output
 
-function printUserData(name, id, language, email) {
+function printUserData(name, id, language) {
 
-	return `Hello World, this is ${name} with HNGi7 ID  ${id} and Email ${email}using ${language} for stage 2`
+	return `Hello World, this is ${name} with HNGi7 ID  ${id} using ${language} for stage 2`
 	
 }
 
-printUserData(interName, internId, programmingLanguage, iternEmail)
+console.log(printUserData(userData.internName, userData.internId, userData.programmingLanguage, ))
+
+
