9e77bffd709860854063e7cd6c1a5ee04e895898
Prechy1211 <66218597+Prechy1211@users.noreply.github.com>
2020-06-04 23:15:59 +0100

    updated and renamed to prechy.js

--- scripts/Prechy.js
+++ scripts/Prechy.js
@@ -3,4 +3,4 @@ let id = "HGN-00208";
 let lang = "Javascript";
 let email = "preciousrobinsonokafor@gmail.com";
 //output should look like this: Hello World, this is Precious Okafor with HGN ID: HGN-00208 using Javascript fot stage two task 
-console.log(`Hello World, this is ${fullName} with HGN ID ${id} and email ${email} using ${lang} for stage 2 task`);
+console.log(`Hello World, this is ${fullName} with HGN ID ${id} using ${lang} for stage 2 task`);
