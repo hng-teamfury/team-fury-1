fb291f643d6da8b88e0ccf943cf361d1c014e5e9
Sorin-tech <66221497+Sorin-tech@users.noreply.github.com>
2020-06-04 16:32:25 +0100

    Update Sorin's Script
    email included

--- Sorin Script.py
+++ Sorin Script.py
@@ -7,8 +7,9 @@
 def myscript():
     name = 'Sorinola Michael'
     ID = 'HNG-02213'
+    email = 'sorin.mykael@gmail.com'
     language = 'python'
-    print(f'Hello World, this is {name} with HNGi7 ID {ID} using {language} for stage 2 task.')
+    print(f'Hello World, this is {name} with HNGi7 ID {ID} using {language} for stage 2 task')
 
 
 # In[2]:
