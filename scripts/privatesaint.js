5a5085ad0df9903b31c98dd0898f0c6af0cab8ac
Gbenga Olowogbayi <olowogbayigbenga@gmail.com>
2020-06-04 22:42:06 +0100

    added email to the text

--- scripts/privatesaint.js
+++ scripts/privatesaint.js
@@ -1,1 +1,1 @@
-console.log("Hello World, this is Joshua Olowogbayi with HNGi7 ID HNG-03066 using Javascript for stage 2 task")
+console.log("Hello World, this is Joshua Olowogbayi with HNGi7 ID HNG-03066 and email olowogbayigbenga@gmail.com using Javascript for stage 2 task")
