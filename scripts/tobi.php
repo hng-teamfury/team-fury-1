5f7ecd8c911e77d688ef0548216d6a238de1f340
tobbey <oluwatobi275@gmail.com>
2020-06-04 15:49:40 +0100

    my script work

---
+++ scripts/tobi.php
@@ -0,0 +1,16 @@
+<?php 
+		function mydetails(){
+
+			$id="HNG-04469";
+			$fullname="Adesina Oluwatobi";
+			$email="oluwatobi275@gmail.com";
+			$language="PHP";
+
+ echo "Hello World, this is ".$fullname." with HNGi7 ID ".$id." and email ".$email." using ".$language." for Stage 2 task";
+
+}
+		  
+  mydetails();
+
+
+?> 
