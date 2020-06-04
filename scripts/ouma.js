6f896616abf01063a445ccaafafacc322517655b
TonyOuma <kyrietony@gmail.com>
2020-06-04 22:06:15 +0300

    fix update on variables

--- scripts/ouma.js
+++ scripts/ouma.js
@@ -1,10 +1,9 @@
-var status = "pass";
-var email = "oumat621@gmail.com";
 // function to output the required details
 function showMyDetails() {
+    var email = "oumat621@gmail.com";
     var id = "HNG-02585";
     var lang = "JavaScript";
     var name = "Tony Ouma";
-    return `Hello World, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task`;
+    console.log(`Hello World, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task`);
 }
 showMyDetails();
