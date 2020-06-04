d9e61f800ea3172bf688132fe2c215427e63a1bb
Francisop <francisohis@gmail.com>
2020-06-04 20:59:26 +0100

    changed id

--- scripts/francis.py
+++ scripts/francis.py
@@ -1,10 +1,10 @@
 import json
 
 name="Francis Onwukwe"
-ID = "03944"
+id = "03944"
 email ="francisohis@gmail.com"
 language = "Python"
 
-data = "Hello World,this is {} with  HNGi7 ID HNG-{} and email {} using {} for stage 2 task".format(name,ID,email,language)
+data = "Hello World,this is {} with  HNGi7 ID HNG-{} and email {} using {} for stage 2 task".format(name,id,email,language)
 json_output = json.dumps(data,indent=1)
 print(json_output)
