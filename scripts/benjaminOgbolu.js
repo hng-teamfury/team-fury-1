61ca19e10e2fde253508f29592d682b5f4a321ef
Benjamin-411 <61962055+Benjamin-411@users.noreply.github.com>
2020-06-04 10:09:01 +0100

    Update benjaminOgbolu.js
    My first created script

--- scripts/benjaminOgbolu.js
+++ scripts/benjaminOgbolu.js
@@ -3,6 +3,6 @@ const id = "HNG-02588";
 const email = "bhennie411@gmail.com";
 const language = "Javascript";
 const myScript = () => {
-    return `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task`
+    return `Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`
 };
 console.log(myScript());
