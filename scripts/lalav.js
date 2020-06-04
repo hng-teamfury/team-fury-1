f8690c7ec0a4daa4baa90009fe6e1d920e9981c1
varlov <51709867+varlov@users.noreply.github.com>
2020-06-04 23:11:47 +0100

    Add files via upload

---
+++ scripts/lalav.js
@@ -0,0 +1,3 @@
+let name = "Rebecca Lalav", id =  HNG-00993", email = "dovelalove@gmail.com", language = "Javascript";
+
+console.log("Hello World, I am " + name + " with HNGi7 ID " + id + " and email " + email + " using " + language + " for stage 2 task")
