671d08f7c55395d20a0cebdac24b8a517e7aad71
Ruby <graceffiong@gmail.com>
2020-06-04 09:45:26 +0100

    Update Ruby.js

--- scripts/Ruby.js
+++ scripts/Ruby.js
@@ -2,6 +2,6 @@ let details = {fullname: "Grace Effiong", ID: "HNG-01818", email: "graceffiong@g
 
 function information() {
     output = "Hello world, this is " + details.fullname + " with HNGi7 ID " + details.ID + " and email " + details.email + " using " + details.language + " for stage 2 task";
-    return output;   
+    console.log(output);   
 }
 information();
