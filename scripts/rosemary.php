5debb184252b6e58645e159f74bad3779b286abf
Rosemary <59313516+Nwosu-Ihueze@users.noreply.github.com>
2020-06-04 04:15:55 -0400

    rosemary's script

---
+++ scripts/rosemary.php
@@ -0,0 +1,25 @@
+<?php
+
+    $fullname = "Rosemary Nwosu-Ihueze";
+   
+
+    $id = "HNG-01572";
+    
+
+    $language = "PHP";
+   
+
+    $email = "nwosunneoma@gmail.com";
+
+   
+   
+   
+    
+
+$result = "Hello World, this is ". $fullname. " with HNGi7 ID " . $id. " using ". $language. " for stage 2 task and email " .$email;
+
+
+
+echo $result
+
+?>
