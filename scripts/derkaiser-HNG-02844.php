53fa56a00986bb2315f9955ba2a8f4be09caf62b
DerKaiser7 <44611064+DerKaiser7@users.noreply.github.com>
2020-06-04 07:43:55 +0100

    updated a variable in my script

--- scripts/derkaiser-HNG-02844.php
+++ scripts/derkaiser-HNG-02844.php
@@ -1,7 +1,7 @@
 <?php
-    $info = array("name"=>"Falodu Stephen Abimbola", "hng_id"=>"HNG-02844", "language"=>"php", "email"=>"stephenfalodu@icloud.com");
+    $info = array("fullname"=>"Falodu Stephen Abimbola", "hng_id"=>"HNG-02844", "language"=>"php", "email"=>"stephenfalodu@icloud.com");
 
-    echo "Hello World, this is " . $info['name'] . " with HNGi7 ID: " . $info['hng_id'] . " using " . $info['language'] . " for stage 2 task";
+    echo "Hello World, this is " . $info['fullname'] . " with HNGi7 ID: " . $info['hng_id'] . " using " . $info['language'] . " for stage 2 task";
 
     echo json_encode ($info);
 
