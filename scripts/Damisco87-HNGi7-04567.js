f714de3715ff1a126f619cca07d49d3fdd263cbf
damisco87 <damisco87@aim.com>
2020-06-04 15:56:44 +0100

    edited my internship ID

--- scripts/Damisco87-HNGi7-04567.js
+++ scripts/Damisco87-HNGi7-04567.js
@@ -1,5 +1,5 @@
 const fullname = "Festus Okunlola";
-const id = "HNG-04567";
+const id = "-04567";
 const language = "javascript";
 const email = "damisco87@aim.com"
 const tasktext = `hello world, this is ${fullname} with the HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`;
