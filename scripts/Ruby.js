c1dc065b304195bfa90f823cf3d54029dd403657
Ruby <graceffiong@gmail.com>
2020-06-04 19:33:01 +0100

    Removed email from output

--- scripts/Ruby.js
+++ scripts/Ruby.js
@@ -1,7 +1,7 @@
 let details = {fullname: "Grace Effiong", ID: "HNG-01818", email: "graceffiong@gmail.com", language: "JavaScript"};
 
 function information() {
-    output = "Hello world, this is " + details.fullname + " with HNGi7 ID " + details.ID + " and email " + details.email + " using " + details.language + " for stage 2 task";
+    output = "Hello world, this is " + details.fullname + " with HNGi7 ID " + details.ID + " using " + details.language + " for stage 2 task";
     console.log(output);   
 }
 information();
