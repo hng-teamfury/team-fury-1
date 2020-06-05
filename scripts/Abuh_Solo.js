89b8e7d8e52f3afcc1294d1f4b61aca37cf1676e
Abuh Sueliman <53456402+Abuh05@users.noreply.github.com>
2020-06-05 17:06:33 +0100

    create_Abuh_Solo.js

---
+++ scripts/Abuh_Solo.js
@@ -0,0 +1,18 @@
+function internShip() {
+  let fullName = "Habu Suleiman";
+  let id = "HNG-05884";
+  let email = "abuhsolo05@gmail.com";
+  let language = "Java script";
+  console.log(
+    "Hello World, This is  " +
+      fullName +
+      "    with HNGi7 ID   " +
+      id +
+      "and Email " +
+      email +
+      "   using  " +
+      language +
+      " for stage 2 task "
+  );
+}
+internShip();
--- scripts/dave-ok.js
+++ scripts/dave-ok.js
@@ -1,1 +1,3 @@
-console.log("Hello World, this is David Okanlawon with HNGi7 ID HNG-04700 and email dave_okanlawon@yahoo.com using NodeJs for stage 2 task");
+console.log(
+  "Hello World, this is David Okanlawon with HNGi7 ID HNG-04700 and email dave_okanlawon@yahoo.com using NodeJs for stage 2 task"
+);
