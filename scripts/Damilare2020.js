fb4aa9f5f6c8668be18ba7994fd932e5e3d185c7
Damilare50 <66230820+Damilare50@users.noreply.github.com>
2020-06-05 08:40:26 +0100

    My updated script

---
+++ scripts/Damilare2020.js
@@ -0,0 +1,9 @@
+// Created Using Easy HTML v1.4.1
+// https://play.google.com/store/apps/details?id=ak.andro.easyhtml
+
+var name = "Oluwadamilare Okubanjo";
+var id = "HNG-02526";
+var language = "Javascript";
+var email = "Damilarelamine@gmail.com";
+
+console.log("Hello World, this is " + name + " with HNGi7 ID " + id + " using " + language + " for stage 2 task");
