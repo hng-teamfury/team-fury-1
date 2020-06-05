0f769a4363259bb12a68ad1258e1496ac61acaf8
Trojan9 <timmybanjo@gmail.com>
2020-06-05 20:09:29 +0100

    new changes to script

--- scripts/TimmyBanjo.py
+++ scripts/TimmyBanjo.py
@@ -1,7 +1,8 @@
 name= "Banjo Oluwatimilehin"
 ID= "HNG-05539"
 language="python"
+email="timmybanjo@gmail.com"
 # a python dictionary
 def returnjson():
-    print("Hello World, this is "+ name +" with HNGI7 ID: "+ ID +" using "+ language +" for stage 2 task")
+    print(f"Hello World, this is {name} with HNGI7 ID: {ID} using {language} for stage 2 task")
 returnjson()
