0d5dc3191e2114ae730752225cfec72ba37b6ab0
Alexis <alexismajyambere2@gmail.com>
2020-06-05 11:03:26 +0200

    add-email-to-script

--- scripts/alexis.js
+++ scripts/alexis.js
@@ -1,10 +1,10 @@
-const displayMyscript = () => {
-    const myInfo = {
-        fullname:"Alexis Majyambere",
-        id:"HNG-00521",
-        language:"JavaScript",
-        email:"alexismajyambere2@gmail.com"
-    }
-    console.log(`Hello World, this is ${myInfo.fullname} with HNGi7 ID ${myInfo.id} using ${myInfo.language} for stage 2 task`);
+const myInfo = {
+    fullname:"Alexis Majyambere",
+    id:"HNG-00521",
+    language:"JavaScript",
+    email:"alexismajyambere2@gmail.com"
 }
-displayMyscript();
+const displayMyscript = (info) => {
+    return `Hello World, this is ${info.fullname} with HNGi7 ID ${info.id} and email ${info.email} using ${info.language} for stage 2 task`;
+}
+console.log(displayMyscript(myInfo));
