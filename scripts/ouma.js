3e845190978fee5485ef611525b684e07b667148
TonyOuma <kyrietony@gmail.com>
2020-06-05 06:20:41 +0300

    fix changes to include email

--- scripts/ouma.js
+++ scripts/ouma.js
@@ -4,6 +4,6 @@ var lang = "JavaScript";
 var name = "Tony Ouma";
 // function to output the required details
 function showMyDetails() {
-    console.log(`Hello World, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task`);
+    console.log(`Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${lang} for stage 2 task `);
 }
 showMyDetails();
