1de827255b140f403ec83f602aeb1eb0bb612efa
Menor25 <66224353+Menor25@users.noreply.github.com>
2020-06-05 23:14:52 +0100

    Update menor.js

--- scripts/menor.js
+++ scripts/menor.js
@@ -4,14 +4,6 @@ var hngId = "HNG-02809";
 var langauge = "javascript";
 var email = "ajirimenor@gmail.com";
 
-var myOutput = "Hello World, i am " + fullName + " with HNGi7 ID " + hngId + " and email " + email + " using " + langauge + " for stage 2 task";
+var myOutput = `Hello World, this is ${fullName} with HNGi7 ID ${hngId} and email ${email} using ${langauge} for stage 2 task`;
 
 console.log(myOutput);
-function menorOutput(){
-    
-   
-    return myOutput;
-}
-
-
-menorOutput();
