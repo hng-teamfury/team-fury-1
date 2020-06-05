891f76b9c5ccb697c778941614612ce1faba2d42
Allistair Vilakazi <allistair.vilakazi@gmail.com>
2020-06-05 23:10:32 +0200

    fix: remove script file
    This file can be safely removed because it exists under the scripts folder as maisha.js

--- maishat.js
+++
@@ -1,7 +0,0 @@
-
-var maishaName = 'Aisha Mustafa'
-var maishaLanguage = 'Javascript'
-var maishaHng = 'HNG-01743'
-var maishaEmail = 'aishaokeisha@gmail.com'
-
-console.log('Hello World, this is ' + maishaName + ' with HNGi7 ID ' + maishaHng + ' and email ' + maishaEmail + ' using ' + maishaLanguage + ' for stage 2 task'); 
