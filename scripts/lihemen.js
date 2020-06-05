893577b952396a9a8f0c2e156ec2eb410cc1c455
Hemen Lan <lihemen@yahoo.com>
2020-06-05 23:30:34 +0100

    Please my script started showing invalid, update!
    New update

--- scripts/lihemen.js
+++ scripts/lihemen.js
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
 const lihemen = {
 	firstname: 'Hemense',
 	lastname: 'Lan',
 	id: 'HNG-03786',
 	email: 'lihemen@gmail.com',
-	stack: 'javascript'
+	stack: 'javascript',
 };
 
 console.log(
@@ -14,21 +13,5 @@ console.log(
 		lihemen.email
 	} using ${lihemen.stack.toUpperCase()} for stage 2 task`
 );
-=======
-let lihemen = {
-	firstname: 'hemense',
-	lastname: 'lan',
-	id: 'HNG-03786',
-	stack: 'javascript',
-	email: 'lihemen@gmail.com',
-	team: 'tean-fury',
-	track: 'backend'
-};
-console.log(
-	`Hello World, this is ${lihemen.firstname.toUpperCase() +
-		' ' +
-		lihemen.lastname.toUpperCase()}, with HNG ID ${
-		lihemen.id
-	}, and email ${lihemen.email}, using ${lihemen.stack.toUpperCase()} for stage 2 task`
-);
->>>>>>> master
+
+
