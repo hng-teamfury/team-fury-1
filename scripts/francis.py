28b36b332bdac0bfa47c2e1de3c974ef6a146df7
Francisop <40735031+Francisop@users.noreply.github.com>
2020-06-05 16:41:40 +0100

    Update francis.py

--- scripts/francis.py
+++ scripts/francis.py
@@ -1,17 +1,10 @@
-import json
-
 name="Francis Onwukwe"
 id = "03944"
 email ="francisohis@gmail.com"
 language = "Python"
-print( f"Hello World,this is {name} with  HNGi7 ID HNG-{id} and email {email} using {language} for stage 2 task")
 
 
-
-def script():
-  data = "Hello World, this is {} with HNGi7 ID {} and email {} using {} for stage 2 task".format(name,id,email,language)
-  output = json.dumps(data)
-  print(output) 
+def script(name,id,email,language):
+    return f"Hello World, this is {name} with HNGi7 ID {id} and email {email} using {language} for stage 2 task"
   
-
-script() 
+print(script(name,id,email,language))
