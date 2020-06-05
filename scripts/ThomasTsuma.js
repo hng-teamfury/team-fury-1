73f236fb0b5c8ab3b55245ffcb616c79b5f06d0e
TomiTsuma <tommytsuma7@gmail.com>
2020-06-05 05:19:35 -0700

    Please merge this

--- scripts/ThomasTsuma.js
+++ scripts/ThomasTsuma.js
@@ -3,7 +3,7 @@ const message = 'Hello World,' ;
 const name = 'Thomas Amogolla Tsuma';
 const HNGi7_ID = 'HNG-00332';
 const language = 'javascript';
-const email = 'tommytsuma7@gmail.com@gmail.com'
+
 
 const User = {
     message:'message',
@@ -12,5 +12,5 @@ const User = {
     language: 'JS',
     email: 'email'
 }
-console.log(message + ' this is '+ name + ' with HNGi7 ID ' + HNGi7_ID + ' and email '+ email + ' using ' + language + ' for stage 2 task')
+console.log(message + ' this is '+ name + ' with HNGi7 ID ' + HNGi7_ID + ' using ' + language + ' for stage 2 task')
 
