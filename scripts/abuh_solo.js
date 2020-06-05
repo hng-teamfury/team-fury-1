a43319bf962cccff46d147ef2e625a4d6e3682b7
Abuh Sueliman <53456402+Abuh05@users.noreply.github.com>
2020-06-05 12:44:29 +0100

    Create abuh_solo.js

---
+++ scripts/abuh_solo.js
@@ -0,0 +1,18 @@
+function HNGi7Profile() {
+  let FullName = "Habu Suleiman";
+  let ID = "HNG-05884 ";
+  let Email = "abuhsolo05@gmail.com";
+  let Language = "Javascript";
+  console.log(
+    "Hello Word, this is " +
+      FullName +
+      "with HNGi7 ID" +
+      ID +
+      "and email " +
+      Email +
+      "using " +
+      Language +
+      "for stage 2 track "
+  );
+}
+HNGi7Profile();
