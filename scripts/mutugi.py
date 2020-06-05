62a346d06a181e62c74b674ca6cefe9d4d755c09
mutugiii <mutugimutuma@gmail.com>
2020-06-05 22:10:04 +0300

    Fix

--- scripts/mutugi.py
+++ scripts/mutugi.py
@@ -3,4 +3,4 @@ hng7_id = "HNG-01414"
 language = "Python"
 email = "billmutuma@gmail.com"
 
-print(f'Hello World, this is {name} with HNGi7 ID {hng_id} and email {email} using {language} for stage 2 task')
+print(f'Hello World, this is {name} with HNGi7 ID {hng7_id} and email {email} using {language} for stage 2 task')
