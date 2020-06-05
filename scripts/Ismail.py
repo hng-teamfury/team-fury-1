4b7f892efd47b6019e0c32d100c5cdedeaf6f9f5
Muni-Ismail <66263275+Muni-Ismail@users.noreply.github.com>
2020-06-05 19:12:42 +0100

    Update Ismail.py

--- scripts/Ismail.py
+++ scripts/Ismail.py
@@ -6,9 +6,13 @@ git commit - a -m "My name is Ismail"
 git push origin [team-fury-1]
 """
 
-Name = "Ismaila Shaibu"
-ID = "HNG-04281"
-email = "Shaibu.ism1@gmail.com"
-language = "Python"
+def myscript():
+    Name = "Ismaila Shaibu"
+    ID = "HNG-04281"
+    email = "Shaibu.ism1@gmail.com"
+    language = "Python"
 
-print(f"Hello World, this is {Name} with HNGi7 ID {ID} and email {email} using {language} for stage 2 task")
+
+    print(f"Hello World, this is {Name} with HNGi7 ID {ID} and email {email} using {language} for stage 2 task")
+
+myscript()
