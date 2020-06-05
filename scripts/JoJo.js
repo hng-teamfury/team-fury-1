b5fd1ae1f1c288ce074d607297424ae0cfede726
jOjO1083 <64544736+jOjO1083@users.noreply.github.com>
2020-06-05 21:58:16 +0100

    Create JoJo.js

---
+++ scripts/JoJo.js
@@ -0,0 +1,11 @@
+let myname = 'Jonathan Joseph Ikpe'
+
+let id = 'HNG-05569'
+
+let programminglanguage = 'PHP'
+
+let myemail = 'jonathan.joseph43@yahoo.com'
+
+printstmt = `Hello World, this is ${name} with HNGi7 ID ${id} and email ${myemail} using ${programminglanguage} for stage 2 task`
+
+console.log(printstmt)
