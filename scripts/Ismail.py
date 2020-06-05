80ea4b52f1ef862b414f44269ba34be373860310
Muni-Ismail <66263275+Muni-Ismail@users.noreply.github.com>
2020-06-05 12:02:41 +0100

    Create Ismail.py

---
+++ scripts/Ismail.py
@@ -0,0 +1,15 @@
+"""git
+git branch [ team-fury-1 ]
+git checkout [team-fury-1]
+git add.
+git commit - a -m "My name is Ismail"
+git push origin [team-fury-1]
+"""
+
+def myscript():
+    Name = "Ismaila Shaibu"
+    ID = "HNG-04281"
+    email = "Shaibu.ism1@gmail.com"
+    language = "Python"
+
+print(f"Hello World, this is {Name} with HNGi7 ID {ID} and email {email} using {language} for stage 2 task")
