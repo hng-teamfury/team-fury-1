8aa65715df96b1f78d757702038a7dcb9f8497cb
Ernest <epospiky@gmail.com>
2020-06-05 23:11:41 +0100

    Updated vixon

--- scripts/vixon.js
+++ scripts/vixon.js
@@ -4,6 +4,6 @@ const email = "epospiky@gmail.com";
 const language = "JavaScript";
 
 const fullDetails = () => {
-	return `Hello, world! This is ${name} with the HNG ID ${id} and email address ${email} using ${language} language for stage 2 promotion`
+	return `Hello, world! This is ${name} with the HNG ID ${id} and email address ${email} using ${language} language for stage 2 task`
 };
 console.log(fullDetails());
