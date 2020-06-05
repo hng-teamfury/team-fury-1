49fcca403480a0aa61b37f5202f407c283cd9829
Rishabh-1936 <rishabhjain.1936@gmail.com>
2020-06-05 17:54:16 +0530

    Script updated

---
+++ scripts/Rishabh.js
@@ -0,0 +1,6 @@
+var fullname = "Rishabh Jain";
+var language = "JavaScript";
+var id = "HNG-03265";
+var email = "rishabhjain.1936@gmail.com"
+
+console.log("Hello World, this is" + " " + fullname + " " + "with HNGi7 ID" + " " + id + " and email " + email + " " + "using" + " " + language + " " + "for stage 2 task ")
