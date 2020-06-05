4fdc0d1b71a6345ca0d417647d2b013d38fe1015
Dave <callremz@gmail.com>
2020-06-05 11:54:13 +0100

    fix: remove stragglers modify name regx

--- moh_elelu.py
+++
@@ -1,6 +0,0 @@
-name = 'Taiye Elelu'
-ID= 'HNG-06732'
-email = 'elelutaiwo2010@yahoo.com'
-program ='python'
-
-print(f'Hello world, this is {name} with HNGi7 ID {ID} and email {email} using {program} for stage 2 task.')
