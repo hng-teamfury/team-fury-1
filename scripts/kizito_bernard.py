4f7e9e475c38f820bea2da4224849747b811acff
BoovyBernard <32014299+BoovyBernard@users.noreply.github.com>
2020-06-05 04:43:12 +0300

    Update kizito_bernard.py

--- scripts/kizito_bernard.py
+++ scripts/kizito_bernard.py
@@ -2,5 +2,5 @@
 #intern_intro = "Hello World, this is " + intern_details[0] + " with HNGi7ID " + intern_details[1] + " using " + intern_details[2] + " for stage 2 task";
 
 intern_details_2 = { 'fullname': 'Kizito Bernard', 'id': '02348', 'email': 'kizitobernard@gmail.com', 'language': 'Python' };
-intern_intro_2 = "Hello World, this is " + intern_details_2['fullname'] + " with HNGi7ID " + intern_details_2['id'] + " using " + intern_details_2['language'] + " for stage 2 task";
+intern_intro_2 = "Hello World, this is " + intern_details_2['fullname'] + " with HNGi7ID " + intern_details_2['id'] + " and " + intern_details_2['email'] + " using " + intern_details_2['language'] + " for stage 2 task";
 print ( intern_intro_2 )
