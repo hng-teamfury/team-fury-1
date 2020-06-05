c8edce3f9098f37fd388e89a5123d9b8c3394dfd
Brown Coder <adesuwahealthalive@gmail.com>
2020-06-06 00:02:04 +0100

    add my script

---
+++ scripts/Adesuwa.js
@@ -0,0 +1,8 @@
+var fullname = "Adesuwa Adolosa";
+ var ID = "HNG-02362";
+ var language = "JavaScript";
+ var email = "adesuwahealthalive@gmail.com";
+
+
+var obj =  console.log(" Hello World, this is " + fullname + " with HNGi7 ID " + ID + " using " + language + " for stage 2 task.");
+var myJson = JSON.stringify(obj);
