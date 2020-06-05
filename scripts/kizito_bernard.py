b927905482c8bf24d675db2e98cbb04bb70022c7
BoovyBernard <32014299+BoovyBernard@users.noreply.github.com>
2020-06-05 21:41:39 +0300

    Update kizito_bernard.py

--- scripts/kizito_bernard.py
+++ scripts/kizito_bernard.py
@@ -4,9 +4,4 @@ email = "kizitobernard@gmail";
 language = "python";
     
 def intern_intro():
-
     print ("Hello World, this is " + str(fullname) + " with HNGi7 ID " + str(iD )+ " and email " + str(email) + " using " + str(language) + " for stage 2 task");
-    
-    return
-    
-intern_intro();
