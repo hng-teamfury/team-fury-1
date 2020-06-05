979f3c11744cf1706918277aa7a7c21d74869ef5
INNOCENT THOMAS <innthomas00158@gmail.com>
2020-06-05 02:19:34 +0100

    adding my script(innthomas.py)

---
+++ scripts/innthomas.py
@@ -0,0 +1,5 @@
+fullName="Innocent Thomas"
+ID="HNG-00678"
+langage="Python"
+email="innthomas00158@gmail.com"
+print ("Hello Word, this is {} with HNGi7 ID {} and email {} using {} for stage 2 task".format(fullName, ID, email, langage))
