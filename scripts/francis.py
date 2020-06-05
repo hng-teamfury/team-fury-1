dab035df5e07147dd638929e62e36b24d7c2f467
Francisop <40735031+Francisop@users.noreply.github.com>
2020-06-05 18:30:24 +0100

    Update francis.py

--- scripts/francis.py
+++ scripts/francis.py
@@ -3,8 +3,6 @@ id = "03944"
 email ="francisohis@gmail.com"
 language = "Python"
 
-
-def script(name,id,email,language):
-    return f'Hello World, this is {name} with HNGi7 ID {id} and email {email} using {language} for stage 2 task'
+print(f'Hello World, this is {name} with HNGi7 ID {id} and email {email} using {language} for stage 2 task')
   
-print(script(name,id,email,language))
+
