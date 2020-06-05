69fde9f3ce7b1e7b76a544c7262a73a94d1a15d1
BoovyBernard <32014299+BoovyBernard@users.noreply.github.com>
2020-06-05 03:31:10 +0300

    Add files via upload

---
+++ scripts/kizito_bernard.py
@@ -0,0 +1,6 @@
+#intern_details = [ "Kizito Bernard", "02348", "Python" ];
+#intern_intro = "Hello World, this is " + intern_details[0] + " with HNGi7ID " + intern_details[1] + " using " + intern_details[2] + " for stage 2 task";
+
+intern_details_2 = { 'fullname': 'Kizito Bernard', 'id': '02348', 'email': 'kizitobernard@gmail.com', 'language': 'Python' };
+intern_intro_2 = "Hello World, this is " + intern_details_2['fullname'] + " with HNGi7ID " + intern_details_2['id'] + " using " + intern_details_2['language'] + " for stage 2 task";
+print ( intern_intro_2 )
