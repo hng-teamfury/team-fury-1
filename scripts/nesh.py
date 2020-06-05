1e6fdf990e4919866a3941159ac658f1734365b3
marknesh <markmunene.nesh@gmail.com>
2020-06-05 11:08:26 +0300

    push for merge

--- scripts/nesh.py
+++ scripts/nesh.py
@@ -1,6 +1,6 @@
 name = 'Mark Munene'
 id = 'HNG-03593'
 language = 'python'
-print('Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task'.format(name,id,language))
+print('Hello World,this is {} with HNGi7 ID {} using {} for stage 2 task'.format(name,id,language))
 
 
