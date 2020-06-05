6c70e10bb1325b219919b058149079debcda48d7
Sophyia7 <66218403+Sophyia7@users.noreply.github.com>
2020-06-05 09:53:32 +0100

    add my script

--- scripts/Sophyia.py
+++ scripts/Sophyia.py
@@ -3,4 +3,4 @@ hng = "HNG-00648"
 email = "iroegbusophia3@gmail.com"
 lang = "Python"
 
-print("Hello World, this is " + name + " with HNGi7 ID " + hng + "and email " + email + " using " + lang + " for stage 2 task")
+print("Hello World, this is " + name + " with HNGi7 ID " + hng + " and email " + email + " using " + lang + " for stage 2 task")
