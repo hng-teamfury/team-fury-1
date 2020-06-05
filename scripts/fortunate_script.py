975d0c22269cc63052c5c7fd8b87dd9577b8c2b2
Omoleye Julius <joshkid610@gmail.com>
2020-06-05 02:46:50 +0100

    add fortunate hello world script

---
+++ scripts/fortunate_script.py
@@ -0,0 +1,9 @@
+def hng_intern():
+    name='Julius Omoleye'
+    email='Joshkid610@gmail.com'
+    hng_id='HNG-00378'
+    language='PYTHON'
+
+    print(f"Hello World, this is {name} with HNGi7 ID {hng_id} and email {email} using {language} for stage 2 task")
+
+hng_intern()
