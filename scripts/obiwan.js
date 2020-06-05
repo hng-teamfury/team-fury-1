af52573a6c765e5bff0c0f1bea4b8620b8c42dde
feyifoluwa <abioyefeyifoluwa@gmail.com>
2020-06-05 07:04:24 +0100

    update obiwan.js

--- scripts/obiwan.js
+++ scripts/obiwan.js
@@ -1,6 +1,8 @@
-let name = "Abioye Feyifoluwa";
-let id = "HNG-00889";
-let language = "JavaScript";
-let email = "abioyefeyifoluwa@gmail.com"
+let obj={
+  name : "Abioye Feyifoluwa",
+  id : "HNG-00889",
+  language : "JavaScript",
+  email : "abioyefeyifoluwa@gmail.com"
+}
 
-console.log(`Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`)
+console.log(`Hello World, this is ${obj.name} with HNGi7 ID ${obj.id} using ${obj.language} for stage 2 task`)
