702bdc647714631361615c75f67e7eba44ef81db
aremcx84 <aremcx84@gmail.com>
2020-06-05 11:19:27 +0100

    aremcx.py

--- scripts/aremcx.py
+++ scripts/aremcx.py
@@ -1,47 +1,6 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 15,
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "Hello World this is Abdul'wakil Jamiu with HNGi7 ID HNG-04287 and email aremcx84@gmail.com using python for stage 2\n"
-     ]
-    }
-   ],
-   "source": [
-    "a = 'Hello World'\n",
-    "fullName= \"Abdul'wakil Jamiu\"\n",
-    "HNGi7_ID = 'HNG-04287'\n",
-    "email = 'aremcx84@gmail.com'\n",
-    "myDetails = '{0} this is {1} with HNGi7 ID {2} and email {3} using python for stage 2'\n",
-    "print(myDetails.format(a, fullName, HNGi7_ID, email))"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.3"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+a = 'Hello World'
+fullName= "Abdul'wakil Jamiu"
+HNGi7_ID = 'HNG-04287'
+email = 'aremcx84@gmail.com'
+myDetails = '{0} this is {1} with HNGi7 ID {2} and email {3} using python for stage 2'
+print(myDetails.format(a, fullName, HNGi7_ID, email))
