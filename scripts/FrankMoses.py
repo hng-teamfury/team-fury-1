55cc8f16e748ee30f52814410d00a1624b020a3a
Frank Moses <55638637+frank9th@users.noreply.github.com>
2020-06-05 17:24:10 +0100

    Reviewed as requested.
    I was told i didn't print out my output, but i have checked and the code is alright. please review and me more specific with what is need. thanks.

--- scripts/FrankMoses.py
+++ scripts/FrankMoses.py
@@ -3,8 +3,6 @@ fullName = "Frank Moses"
 id = "HNG-06071"
 language = "Python"
 Email = "frankakpore@gmail.com"
-
 def FrankMoses():
   print("Hello World, this is " + fullName + " with HNGi7 ID " + id + " and email " + Email + " using " + language + " for stage 2 task")
-
 FrankMoses()
