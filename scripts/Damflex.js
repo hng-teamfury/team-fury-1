35fcd7ba21a8a1e6c7cf678be4b922970f8b539b
Damflex <akanji4destiny@gmail.com>
2020-06-05 11:41:55 +0100

    Improved Damflex.js

---
+++ scripts/Damflex.js
@@ -0,0 +1,7 @@
+
+ var name = 'Oke Oluwatobi'
+ var id = 'HNG-02356'
+ var email = 'akanji4destiny@gmail.com'
+ var lang = 'Javascript'
+
+ console.log('Hello world, this is ' + name + ' with HNGi7 ID ' + id + ' and email ' + email + ' using ' + lang + ' for stage 2 task')
