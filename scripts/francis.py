21d8102ccb31485fc724431b0299090ae8c34e60
Francisop <40735031+Francisop@users.noreply.github.com>
2020-06-05 22:13:20 +0100

    Revert "Francis2"

--- scripts/francis.py
+++ scripts/francis.py
@@ -1,10 +1,9 @@
+import json
+
 name="Francis Onwukwe"
 id = "03944"
 email ="francisohis@gmail.com"
 language = "Python"
 
+ print(f'Hello World, this is {name} with HNGi7 ID {id} and email {email} using {language} for stage 2 task')
 
-def script(name,id,email,language):
-    return f"Hello World, this is {name} with HNGi7 ID {id} and email {email} using {language} for stage 2 task"
-  
-print(script(name,id,email,language))
