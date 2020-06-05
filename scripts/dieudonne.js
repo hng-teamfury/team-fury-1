2ace87abfe877e16c2408dafc582ec6ce7e9de0b
juniorndem <66252945+juniorndem@users.noreply.github.com>
2020-06-05 23:18:45 +0100

    Update dieudonne.js

--- scripts/dieudonne.js
+++ scripts/dieudonne.js
@@ -1,8 +1,2 @@
-var fullName="NDEM à NDEM Dieudonné", ID="HNG-00799", langage="JavaScript",email="ndemjunior@gmail.com", text1, text2, text3, text4, text5, text;
-text1 = "Hello Word, this is ";
-text2 = " with HNGi7 ID ";
-text3 = " using ";
-text4 = " and ";
-text5 = " for stage 2 task";
-text = text1 + fullName + text2 + ID + text3 + langage + text4 + email + text5;
-alert(text);
+var fullName='NDEM à NDEM Dieudonné', ID='HNG-00799', langage='JavaScript';
+console.log("Hello Word, this is", fullName, "with HNGi7 ID", ID, "using", langage, "for stage 2 task");
