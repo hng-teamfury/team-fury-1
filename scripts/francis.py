31abad5184e09f5b95553beba43299a764e73b25
Francisop <40735031+Francisop@users.noreply.github.com>
2020-06-05 22:28:28 +0100

    Revert "update francis.py"

--- scripts/francis.py
+++ scripts/francis.py
@@ -4,5 +4,6 @@ id = "03944"
 email ="francisohis@gmail.com"
 language = "python"
 
- print(f'Hello World, this is {name} with HNGi7 ID {id} and email {email} using {language} for stage 2 task')
-
+data = "Hello World,this is {} with  HNGi7 ID HNG-{} and email {} using {} for stage 2 task".format(name,id,email,language)
+json_output = json.dumps(data,indent=1)
+print(json_output)
