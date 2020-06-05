992e782761b8e1a1315e9f33d7b0daa466e7723a
Francisop <40735031+Francisop@users.noreply.github.com>
2020-06-05 12:04:00 +0100

    Update francis.py

--- scripts/francis.py
+++ scripts/francis.py
@@ -5,5 +5,5 @@ id = "03944"
 email ="francisohis@gmail.com"
 language = "Python"
 
- print(f'Hello World, this is {name} with HNGi7 ID {Id} and email {email} using {language} for stage 2 task')
+ print(f'Hello World, this is {name} with HNGi7 ID {id} and email {email} using {language} for stage 2 task')
 
