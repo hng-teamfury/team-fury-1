60627ce1a2bccb2502fa72459b7b7ba708636e21
69codes <46348895+69codes@users.noreply.github.com>
2020-06-05 11:27:16 +0100

    na God go forgive me for my mistakes

--- scripts/akuragba1.js
+++ scripts/akuragba1.js
@@ -2,5 +2,5 @@ let name = "Taiwo Yusuf Olatunji";
 let email = "taiwoyusuf24@gmail.com";
 let ID = "HNG-03750";
 let language = "JavaScript";
-var result = "Hello world, " + "this is " + name + " with HNG i7 ID " + ID + " and email " + email + " using " + language  + " for stage 2 task";
+var result = "Hello world, " + "this is " + name + " with HNGi7 ID " + ID + " and email " + email + " using " + language  + " for stage 2 task";
 console.log(result);
