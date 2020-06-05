ce22c791f8a180192ccac00e1a20be4f15ea8d65
Ekaji <45511914+Ekaji@users.noreply.github.com>
2020-06-05 16:19:20 +0100

    Update ekaji.js

--- scripts/ekaji.js
+++ scripts/ekaji.js
@@ -4,7 +4,7 @@
    let email = "ekajionah@gmail.com";
 
 const printBio = () => {
-  let bio = `Hello World, this is ${fullname} with HNGi7 ID ${id} using ${language} for stage 2 task`;
+  let bio = `Hello World, this is ${fullname} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`;
 console.log(bio)
 }
 
