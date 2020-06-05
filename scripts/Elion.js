83f16da2b57aa85e456eb2bd9eb40260560cd0ee
Igweokwu Ekene Leonard <62545645+Elion-commerce@users.noreply.github.com>
2020-06-05 01:32:12 +0100

    Update Elion.js

--- scripts/Elion.js
+++ scripts/Elion.js
@@ -1,11 +1,11 @@
-const full_name= "Igweokwu Leonard Ekene";
+const fullname= "Igweokwu Leonard Ekene";
 const ID= "HNG-06551";
 const email="Ekeneigweokwu@gmail.com";
 const language= "Java Script";
 
 function my(){
  
-    const E_talk= `Hello World, this is ${full_name} with HNGi7 ID ${ID} and email ${email} using ${language} for stage 2 task`;
+    const E_talk= `Hello World, this is ${fullname} with HNGi7 ID ${ID} and email ${email} using ${language} for stage 2 task`;
 
     return E_talk;
 }
