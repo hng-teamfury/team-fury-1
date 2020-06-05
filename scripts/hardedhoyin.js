f58c221fa4a4f4431827926e607403d9f89795e1
Allistair Vilakazi <allistair.vilakazi@gmail.com>
2020-06-05 23:07:15 +0200

    fix: move script file to script folder

---
+++ scripts/hardedhoyin.js
@@ -0,0 +1,7 @@
+const newTask = (name, id, email) => {
+    let output = `Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using Javascript for stage 2 task`;
+  
+    return output;
+  };
+
+  newTask("SamuelSagnele", "HNG-05089", "samuelsagbele@gmail.com");
