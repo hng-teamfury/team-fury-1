7e7eb5fb8f5031bac629242c04d6b4173cdc97c7
Zinniee <eiroegbu97@gmail.com>
2020-06-05 08:16:27 +0100

    add my script

--- scripts/Zinnie.js
+++ scripts/Zinnie.js
@@ -1,9 +1,8 @@
-<script>
+
     const fullName = "Ezinne Iroegbu";
     const hngId = "HNG-01702";
     const email = "eiroegbu97@gmail.com"
-    const language = "Javacript";
+    const language = "Javascript";
 
-console.log('Hello World, this is ${fullName} with HNGi7 ID ${hngId} and email {email} using ${language} for stage 2 task');
+console.log("Hello World, this is" + " " + fullName + " " + "with HNGi7 ID" + " " + hngId + " " + "and email" + " " + email + " " + "using" + " " + language + " " + "for stage 2 task");
     
-</script>
