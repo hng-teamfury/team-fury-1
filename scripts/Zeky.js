093f929303313188bb232a8d374af85de5fc18ee
Z3k9 <ajelelade@gmail.com>
2020-06-05 11:14:22 +0100

    corrected errors

--- scripts/Zeky.js
+++ scripts/Zeky.js
@@ -3,7 +3,7 @@ const hngId ='HNG-01539';
 const language = 'JavaScript';
 const email = 'ajelelade@gmail.com';
 const result = (fullName,hngId,language) => {
-    return `Hello World, this is ${fullName} with HNGi7 ID ${hngId} using ${language} for stage 2 task`;
+    return `Hello World, this is ${fullName} with HNGi7 ID ${hngId} and email ${email} using ${language} for stage 2 task`;
 }
 
 console.log(result(fullName, hngId, language));
