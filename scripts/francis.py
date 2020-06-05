0cb95a1e49fec469eb778fdc202d9ddaac4380ae
Francisop <40735031+Francisop@users.noreply.github.com>
2020-06-05 22:14:25 +0100

    Update francis.py

--- scripts/francis.py
+++ scripts/francis.py
@@ -1,9 +1,8 @@
-import json
 
-name="Francis Onwukwe"
+name="francis onwukwe"
 id = "03944"
 email ="francisohis@gmail.com"
-language = "Python"
+language = "python"
 
  print(f'Hello World, this is {name} with HNGi7 ID {id} and email {email} using {language} for stage 2 task')
 
