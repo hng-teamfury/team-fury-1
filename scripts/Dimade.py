970db48c26cd0999f88a2095f7e3b1b58d18f65c
Crestward <65926941+Crestward@users.noreply.github.com>
2020-06-05 17:22:13 +0100

    Made corrections

--- scripts/Dimade.py
+++ scripts/Dimade.py
@@ -1,10 +1,6 @@
-fullname = "Oladimeji Olamide Adeyemi"
+full_name = "Oladimeji Olamide Adeyemi"
 ID = "HNG-02518"
 email = "oladimeji759@gmail.com"
 language = "Python"
 
-def myScript(fullname, ID, language):
-    output = print ("Hello world, this is " + fullname + " with HNDi7 ID " + ID + " using " + language + " for stage 2 task.")
-    return output
-
-myScript(fullname, ID, language)
+print (f"Hello world, this is {full_name} with HNDi7 ID {ID} using {language} for stage 2 task.")
