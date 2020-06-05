01a4548c2e9d44799636a58eb3c4e5ce9d6840e4
Ruffychris <66364061+Ruffychris@users.noreply.github.com>
2020-06-05 08:08:49 +0000

    Username changed to one word

--- scripts/Rufus Christian.js
+++ scripts/Rufus Christian.js
@@ -2,4 +2,4 @@ function hnginternship(name, id, language, email){
     console.log(`Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`)
 }
 
-hnginternship("Rufus Christian", "HNG-06353", "JavaScript", "christian.rufus23@gmail.com" )
+hnginternship("Rufus", "HNG-06353", "JavaScript", "christian.rufus23@gmail.com" )
