a1cb7eea2e49f2cb180f9c220dfb34e85ea51bdf
marknesh <markmunene.nesh@gmail.com>
2020-06-05 22:08:34 +0300

    push my script

--- scripts/nesh.py
+++ scripts/nesh.py
@@ -1,11 +1,9 @@
-import json
+name = "Mark Munene"
+ID = "HNG-03593"
+lang = "PYTHON"
+Email = "markmunene.nesh@gmail.com"
 
-hng_03593 = {
-    "name": "Mark Munene",
-    "id": "HNG-03593",
-    "language": "Python",
-}
-dump_details = json.dumps(hng_03593)
-load_details = json.loads(dump_details)
+def stage2():
+  print("Hello World, this is " + name + " with HNGi7 ID " + ID + " and email " + Email + " using " + lang + " for stage 2 task")
 
-print ('Hello World, this is %s with HNGi7 ID %s using %s for stage 2 task ' % (load_details['name'], load_details['id'], load_details['language']))
+stage2()
