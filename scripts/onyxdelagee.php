6970f7e2eac2db36b12ec8a996834a5925e6a2de
Onyxdelagee <onyxdelagee@yahoo.com>
2020-06-05 09:58:54 +0200

    onyxdelagee-HNG-00136 script review final

--- scripts/onyxdelagee.php
+++ scripts/onyxdelagee.php
@@ -2,9 +2,8 @@
   $fullName = "Onyeka Eneanya";
   $hngI7Id = "HNG-00136";
   $languageUsed = "PHP";
-  $stage = "stage 1";
+  $stage = "stage 2";
 
   echo "Hello World, this is {$fullName} with HNGi7 ID {$hngI7Id} using {$languageUsed} for {$stage} task.";
-
 
 ?>
