70c6c78f3b2d6433868a6b5674affc33bbaa9e40
Chigbo Raph <52833174+chigboraphael@users.noreply.github.com>
2020-06-05 17:34:06 +0100

    Add files via upload

--- scripts/Rafael.js
+++ scripts/Rafael.js
@@ -3,7 +3,7 @@ var id = "HNG-05090";
 var email = "chigboraphael@gmail.com";
 var language = "Javascript"
 
-var output = "Hello world, this is " + fullName + " with HNGi7 ID " + id + " email " + email + " using " + language + " for stage 2 task"
+var output = "Hello World, this is " + fullName + " with HNGi7 ID " + id + " and email " + email + " using " + language + " for stage 2 task"
 
 function display(){
     return output;
