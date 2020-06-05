658e7bb7fc4faf9be341750645d69925aaf8e2b8
fkelonthebeat <63163936+fkelonthebeat@users.noreply.github.com>
2020-06-05 17:48:18 +0100

    Update fkelonthebeat-HNG-06361.js

--- scripts/fkelonthebeat-HNG-06361.js
+++ scripts/fkelonthebeat-HNG-06361.js
@@ -1,5 +1,5 @@
-let fullName = "Fredrick Ochiagha";
+let name = "Fredrick Ochiagha";
 let id = "HNG-06361";
 let language = "Javascript";
 let email = "chidiebubeochiagha31@gmail.com";
-console.log(`Hello World, this is ${fullName} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`);
+console.log(`Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`);
