16662412f8fc7d683368169fb1c3e94acd7be805
Elvis-1 <igieborelvis@gmail.com>
2020-06-05 08:47:46 -1200

    Added Elvis.js

---
+++ scripts/elvis.js
@@ -0,0 +1,9 @@
+
+const fullName = 'Elvis Igiebor';
+const ID = 03585;
+const email = 'igieborelvis@gmail.com';
+const language = 'Javascript';
+
+alert(`Hello World, this is ${fullName} with HNGi7 ${ID} and email ${email} using ${language} for stage 2 task`);
+
+
