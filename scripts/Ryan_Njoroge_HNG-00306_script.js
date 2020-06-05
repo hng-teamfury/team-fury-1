e4b4d4097eaa6c2e1c3f33987a70db0ad2eb1e29
ryann254 <keddelyronjoz@gmail.com>
2020-06-05 09:02:44 +0300

    HNG-00306 fixed the format

--- scripts/Ryan_Njoroge_HNG-00306_script.js
+++ scripts/Ryan_Njoroge_HNG-00306_script.js
@@ -1,5 +1,6 @@
 const fullName = "Ryan Njoroge";
 const language = "Javascript";
 const email = "keddelyronjoz@gmail.com";
-console.log(`Hello World, this is ${fullName} with HNGi7 ID HNG-00306 and email ${email} using ${language} for
-stage 2 task`);
+console.log(
+  `Hello World, this is ${fullName} with HNGi7 ID HNG-00306 and email ${email} using ${language} for stage 2 task`
+);
