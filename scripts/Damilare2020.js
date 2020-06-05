c3face33d407f1078882cf0d2a5613ff981d00b8
Damilare50 <66230820+Damilare50@users.noreply.github.com>
2020-06-05 09:57:48 +0100

    Update Damilare2020.js

--- scripts/Damilare2020.js
+++ scripts/Damilare2020.js
@@ -3,4 +3,4 @@ var id = "HNG-02526";
 var language = "Javascript";
 var email = "Damilarelamine@gmail.com";
 
-console.log("Hello World, this is " + name + " with HNGi7 ID " + id + " using " + language + " for stage 2 task");
+console.log("Hello World, this is " + name + " with HNGi7 ID " + id + " and email " + email + " using " + language + " for stage 2 task");
