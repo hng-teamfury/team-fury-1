b531b96c54cc4c437490597b17aaefe4e814a53a
BoovyBernard <32014299+BoovyBernard@users.noreply.github.com>
2020-06-05 22:46:51 +0300

    Update Request HNiG Script-fury
    Update Request HNiG Script-fury

--- scripts/kizito_bernard.py
+++ scripts/kizito_bernard.py
@@ -1,7 +1,9 @@
-fullname = "Kizito Bernard";
-iD = "HNG-02348";
-email = "kizitobernard@gmail";
-language = "python";
+fullname = "Kizito Bernard"
+iD = "HNG-02348"
+email = "kizitobernard@gmail"
+language = "python"
+
+def inter_intro(fullname, iD, email, language):
+    print("Hello World, this is  {}  with HNGi7 ID {} and email {} using {} for stage 2 task".format(fullname,iD,email,language))
     
-def intern_intro():
-    print ("Hello World, this is " + str(fullname) + " with HNGi7 ID " + str(iD )+ " and email " + str(email) + " using " + str(language) + " for stage 2 task");
+inter_intro(fullname, iD, email, language)
