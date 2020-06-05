a37744ef9e1625ab4f09b53c823fbce5029e4327
Augustine <aibe64@yahoo.com>
2020-06-05 12:52:07 +0100

    Delete aibe64.js

--- scripts/aibe64.js
+++
@@ -1,4 +0,0 @@
-const profile = ([fullName, hngID, email, lang]) => {
-  return `Hello World, this is ${fullName} with HNGi7 ID ${hngID} and email ${email}, using ${lang} for stage two task.`
-}
-profile(['Ibe Augustine Chiehiura', 'HNG-02557', 'ibea37@gmail.com', 'JavaScript'])
