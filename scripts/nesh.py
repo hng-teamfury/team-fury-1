280c24c747379f9db349245fa0a016b461693f29
marknesh <markmunene.nesh@gmail.com>
2020-06-05 11:50:51 +0300

    merge request

--- scripts/nesh.py
+++ scripts/nesh.py
@@ -8,4 +8,4 @@ hng_03593 = {
 dump_details = json.dumps(hng_03593)
 load_details = json.loads(dump_details)
 
-print ('Hello World, this is %s with HNGi7 ID %s using %s for stage 2 task ' % (load_details['name'], load_details['id'], load_details['language']))
+print ('Hello World,this is %s with HNGi7 ID %s using %s for stage 2 task ' % (load_details['name'], load_details['id'], load_details['language']))
