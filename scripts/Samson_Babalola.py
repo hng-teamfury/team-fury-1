8c4c494bb503b25194aa13f6b0c262d0400d8979
Babalola Samson <smartbabs02@gmail.com>
2020-06-05 08:06:31 +0100

    Added email to output

--- scripts/Samson_Babalola.py
+++ scripts/Samson_Babalola.py
@@ -2,4 +2,4 @@ full_name = 'Samson Babalola'
 email = 'smartbabs02@gmail.com'
 hngi7_id = 'HNG-06555'
 language = 'Python'
-print(' Hello World,this is {} with HNGi7 ID {} and email {} using {} for stage2 task'. format(full_name,hngi7_id,emaillanguage))
+print(' Hello World,this is {} with HNGi7 ID {} and email {} using {} for stage2 task'. format(full_name,hngi7_id,email,language))
