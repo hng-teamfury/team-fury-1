7ab8213ec85e3b6c5f18cced7bfe06a66aa1377a
bvadegboye <64808662+bvadegboye@users.noreply.github.com>
2020-06-05 09:24:44 +0100

    PLEASE MERGE MY NEW COMMIT

--- scripts/Victoria.js
+++ scripts/Victoria.js
@@ -1,6 +1,6 @@
 let fullName = "Victoria Adegboye";
 let id = "HNG-00157";
-let language = "JavaScript";
+let language = "Javascript";
 let email = "bv.adegboye@gmail.com";
-let output = `Hello World, this is ${fullName} with HNGi7 id ${id} using ${language} for stage 2 task`;
-console.log(output);
+console.log(`Hello World, this is ${fullName} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`);
+
