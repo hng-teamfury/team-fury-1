160a3ef52c8ef4d3d3cfdd1e4f5e62f1c34754f8
marknesh <markmunene.nesh@gmail.com>
2020-06-05 11:47:55 +0300

    push puthon script

--- scripts/nesh.py
+++ scripts/nesh.py
@@ -3,10 +3,9 @@ import json
 hng_03593 = {
     "name": "Mark Munene",
     "id": "HNG-03593",
-    "email": "markmunene.nesh@gmail.com",
     "language": "Python",
 }
 dump_details = json.dumps(hng_03593)
 load_details = json.loads(dump_details)
 
-print ('Hello World, this is %s with HNGi7 ID %s and email %s using %s for stage 2 task ' % (load_details['name'], load_details['id'], load_details['email'], load_details['language']))
+print ('Hello World, this is %s with HNGi7 ID %s using %s for stage 2 task ' % (load_details['name'], load_details['id'], load_details['language']))
