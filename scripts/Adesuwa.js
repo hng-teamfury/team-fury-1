837d1f38290c70e8425ec7d83b1b7710d27860d9
Brown Coder <adesuwahealthalive@gmail.com>
2020-06-05 21:07:59 +0100

    add my script

---
+++ scripts/Adesuwa.js
@@ -0,0 +1,5 @@
+let name= "Adesuwa Adolosa";
+let id= "HNG-02362";
+let language= "Javascript";
+let email = "adesuwahealthalive@gmail.com";
+console.log(`Hello World, this is ${name} with HNGi.7 ID ${id} and email ${email} using ${language} for stage 2 task`)
