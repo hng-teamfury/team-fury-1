fc1c745ec727ef45a4f42be06324db58cc91e72b
Wilsona4 <50172975+Wilsona4@users.noreply.github.com>
2020-06-05 11:20:58 +0100

    Create Wilsona4.java
    Please merge my script

---
+++ scripts/Wilsona4.java
@@ -0,0 +1,14 @@
+package scripts;
+
+public class Wilsona4 {
+    public static void main(String[] args) {
+        String fullName = "Ahanmisi Wilson";
+        String ID = "HNG-02720";
+        String email = "wilsonahanmisi@gmail.com";
+        String language = "Java";
+
+        System.out.println("Hello World, this is " + fullName + " with HNGi7 ID " + ID +
+                " and email " + email + " using " + language + " for stage 2 task");
+    }
+    
+}
