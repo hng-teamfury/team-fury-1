e954c82e2ce37fc863b7e8cf5009f05e1cf14807
Adek Niyi <adekniyi@gmail.com>
2020-06-04 19:45:01 -0700

    updated commit

--- scripts/AdekNiyi.js
+++ scripts/AdekNiyi.js
@@ -5,7 +5,7 @@ function myfunction() {
         language:"JavaScript",
         hng_id:"HNG-03886"
     }
-    return `Hello World, this is ${myData.fullName} with HNGi7 ID ${myData.hng_id} and email ${myData.email} using ${myData.language} for stage 2 task`                                                                                                                                                       
+    return `Hello World, this is ${myData.fullName} with HNGi7 ID ${myData.hng_id} using ${myData.language} for stage 2 task`                                                                                                                                                       
 
 }
 
