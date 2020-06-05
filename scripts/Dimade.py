d8a1c433111a6252031152468a9a49dfebac6582
Crestward <65926941+Crestward@users.noreply.github.com>
2020-06-05 18:17:17 +0100

    Updated my script

---
+++ scripts/Dimade.py
@@ -0,0 +1,9 @@
+def myScript():
+    full_name = "Oladimeji Olamide Adeyemi"
+    ID = "HNG-02518"
+    email = "oladimeji759@gmail.com"
+    language = "Python"
+
+    print (f"Hello world, this is {full_name} with HNDi7 ID {ID} using {language} for stage 2 task.")
+
+myScript()
