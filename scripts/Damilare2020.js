e34d71ba8e4270fb80e2feaf65dbeee21cd1d071
Damilare50 <66230820+Damilare50@users.noreply.github.com>
2020-06-05 08:50:15 +0100

    Update Damilare2020.js

--- scripts/Damilare2020.js
+++ scripts/Damilare2020.js
@@ -1,10 +1,6 @@
+var name = "Oluwadamilare Okubanjo";
+var id = "HNG-02526";
+var language = "Javascript";
+var email = "Damilarelamine@gmail.com";
 
-var data = {
-    fullName:"Oluwadamilare Okubanjo",
-    id:"HNG-02526",
-    email:"Damilarelamine@gmail.com",
-    language:"JavaScript"
-};
-var x = "Hello World, this is " + data.fullName + " with HNGi7 ID " + data.id + " using " + data.language + " for stage 2 task";
-console.log(x);
-
+console.log("Hello World, this is " + name + " with HNGi7 ID " + id + " using " + language + " for stage 2 task");
