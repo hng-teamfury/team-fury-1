f2ab9c97800c17da41995ee58e3768f6c0129f96
bori7 <32014655+bori7@users.noreply.github.com>
2020-06-05 18:30:56 +0100

    Update Oluwabori-Ola.py

--- scripts/Oluwabori-Ola.py
+++ scripts/Oluwabori-Ola.py
@@ -1,2 +1,8 @@
 
-print("Hello World, this is Oluwabori Ola with HNGi7 ID HNG-01453 using Python for stage 2 task")
+Name="Oluwabori Ola"
+ID = "HNG-01453"
+Email ="oluwaboriife@gmail.com"
+Lang = "Python"
+
+print(f'Hello World, this is {Name} with HNGi7 ID {ID} and email {Email} using {Lang} for stage 2 task')
+
