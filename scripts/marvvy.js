12bb837702a508fd9fd801eeacd2e4a4d13b9bb9
Marvvy-G <57817706+Marvvy-G@users.noreply.github.com>
2020-06-05 03:31:24 +0100

    Create marvvy.js

---
+++ scripts/marvvy.js
@@ -0,0 +1,9 @@
+var details =
+{
+    name: "Etukudoh Marvellous",
+    hngID: 5011,
+    language: "javascript"
+}
+console.log(
+    "Hello World, this is " + details.name + " with HNGi7 ID " + details.hngID + " and email: babyroyal222@gmail.com using " + details.language + " for stage 2 task"
+)
