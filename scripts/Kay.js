e98f2fcf299eb29f74e1ae81057115dc996f8ea1
Attah Kelechi <51725583+kaycce@users.noreply.github.com>
2020-06-06 20:18:49 +0100

    Update Kay.js

--- scripts/Kay.js
+++ scripts/Kay.js
@@ -2,4 +2,4 @@ const Name = "Attah Kelechi Nathan"
     const HNGi7ID = "HNG-00363"
     const Language = "Javascript"
     const Email = "Kelechinathan04@gmail.com"
-    console.log(`Hello World, this is ${Name} with HNGi7 ID ${HNGi7ID} using ${Language} for stage 2 task`)
+    console.log(`Hello World, this is ${Name} with HNGi7 ID ${HNGi7ID} and email ${Email} using ${Language} for stage 2 task`)
