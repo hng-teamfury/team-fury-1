c42af67e32892482ffa23c56cfefe1e18d294bc2
ovosphere <preciousovobite@gmail.com>
2020-06-06 23:37:47 +0100

    Create newovosphere.js

---
+++ newovosphere.js
@@ -0,0 +1,12 @@
+let name = 'Ovobite Precious';
+let id = 'HNG-02361';
+let email = 'preciousovobite@gmail.com';
+let language = 'Javascript';
+
+let output = "Hello World, this is " + name + " with HNGi7 " + id + " and email " + email + " using " + language + " for stage 2 task";
+
+function displayOutput() {
+    return output;
+}
+
+displayOutput();
