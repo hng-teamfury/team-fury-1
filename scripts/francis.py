b61027c0e133a270f2bd806e90594364539fa09f
Francisop <francisohis@gmail.com>
2020-06-06 13:33:08 +0100

    corrected

--- scripts/francis.py
+++ scripts/francis.py
@@ -1,9 +1,9 @@
-def script():
-    name="francis onwukwe"
-    hngid = "03944"
-    email ='francisohis@gmail.com'
-    language = "python"
+def hng():
+    name='Francis Onwukwe'
+    email='francisohis@gmail.com'
+    hng_id='HNG-03944'
+    language='PYTHON'
 
-    print(f'Hello World, this is {name}, with HNGi7 ID: {hngid} using {language} for stage 2 task.')
+    print(f"Hello World, this is {name} with HNGi7 ID {hng_id} and email {email} using {language} for stage 2 task")
 
-script()
+hng()
