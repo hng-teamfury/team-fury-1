2bcbe6c286a4aa5f3fdff6ee0f963e5ab62c2a08
AlexJake660 <uwaobayagbona@gmail.com>
2020-06-06 11:41:10 +0100

    add script for AlexUwa

---
+++ scripts/AlexUwa.js
@@ -0,0 +1,6 @@
+let hngId = "HNG-00778";
+let email = "uwaobayagbona@gmail.com";
+let langUsed = "JavaScript";
+let fullName = "Obayagbona Uwagbae Alexander";
+
+console.log("Hello world, this is " + fullName + ", with the HNGi7 ID of " + hngId + ", and my email is " + email + ". And I am using " + langUsed + " for the stage 2 task"); 
