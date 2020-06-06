d3bebfe023c8221299483c1568f3bcca2d1239ff
Abuh Sueliman <53456402+Abuh05@users.noreply.github.com>
2020-06-06 02:19:00 +0100

    Update Abuh_Solo.js

--- scripts/Abuh_Solo.js
+++ scripts/Abuh_Solo.js
@@ -3,8 +3,7 @@ function internShip() {
   let ID = "HNG-05884";
   let Email = "abuhsolo05@gmail.com";
   let Language = "Javascript";
-  console.log(
-    `Hello World, this is ${FullName} with HNGi7 ID ${ID} and email ${Email} using ${Language} for stage 2 task`
+  console.log(`Hello World, this is ${FullName} with HNGi7 ID ${ID} and email ${Email} using ${Language} for stage 2 task`
   );
 }
 internShip();
