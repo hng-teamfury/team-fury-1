06e4250b94807bcfc6003215ce1ce3f6e2ec547b
favour <ututufavour6@gmail.com>
2020-06-06 14:29:20 -0700

    my commit

---
+++ scripts/favzz.js
@@ -0,0 +1,7 @@
+var userInfo ={
+    fullName: "Ututu Favour",
+    HNGI7ID:"HNG-03742",
+    email: "ututufavour6@gmail.com",
+    language: "javascript"
+};
+console.log(`this is ${userInfo.fullName} with HNG17 ID ${userInfo.HNGI7ID} and email ${userInfo.email} using ${userInfo.language} for stage 2 task`);
