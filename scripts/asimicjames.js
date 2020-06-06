6058e1cdb12b3488c466e6b7445445825058f182
piouson <piouson@gmail.com>
2020-06-06 18:12:54 +0100

    Fix scripts in wrong directory
    Resolve #739

--- scripts/asimicjames.js
+++ scripts/asimicjames.js
@@ -1,11 +1,11 @@
-function message() {
-    fullName = 'Asimic James Uka';
-    id = 'HNG-00542';
-    email = 'Jayasimic@gmail.com';
-    language = 'JavaScript';
-    console.log('Hello World, this is ' + fullName +
-        ' with HNGi7-ID ' + id + ' and email ' +
-        email + ' using ' + language +
-        ' for Stage 2 task');
+function internInfo() {
+  const fullName = 'Asimic James Uka';
+  const id = 'HNG-00542';
+  const email = 'Jayasimic@gmail.com';
+  const language = 'JavaScript';
+  console.log(' Hello World, this is ' + fullName +
+    ' with HNGi7-ID ' + id + ' and email ' +
+    email + ' using ' + language +
+    ' for Stage 2 task. ');
 }
-message();
+internInfo();
