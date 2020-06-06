216fb82ba9387cbefccfc40759e5c5130841d02b
adeoyebrass <52764075+adeoyebrass@users.noreply.github.com>
2020-06-06 20:43:08 +0100

    Add files via upload

--- scripts/israel.js
+++ scripts/israel.js
@@ -1,11 +1,11 @@
 const sendMessage = () => {
-     const me = {
-	name: 'oluwafemi adeoye',
-	id: 'HNG-04195',
-	email: 'adeoyefemi3@gmail.com',
-	language: 'JavaScript'
-	}
-      const { name, id, email, language } = me;
-      return `Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`
+    const me = {
+   name: 'oluwafemi adeoye',
+   id: 'HNG-04195',
+   email: 'adeoyefemi3@gmail.com',
+   language: 'JavaScript'
+   }
+     const { name, id, email, language } = me;
+     return `Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`
 }
 console.log(sendMessage())
