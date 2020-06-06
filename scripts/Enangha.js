67cde1d22e4901c4ef66551c163eef94369bf654
Godfrey Jonas <41231839+jonasgodfrey@users.noreply.github.com>
2020-06-06 15:29:34 +0100

    Added my file

---
+++ scripts/Enangha.js
@@ -0,0 +1,11 @@
+let mynames = 'Godfrey Jonas'
+
+let hngid = 'HNG-04007'
+
+let lang = 'JavaScript'
+
+let myemail = 'hello@jonasgodfrey.com'
+
+let myInfo = `Hello World, this is ${mynames} with HNGi7 ID ${hngid} and email ${myemail} using ${lang} for stage 2 task`
+
+console.log(myInfo) 
