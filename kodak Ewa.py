386d0d49f1095cb791dcf258f742961fa4f67fda
Ewamiee <66280593+Ewamiee@users.noreply.github.com>
2020-06-06 19:34:03 +0100

    Delete kodak Ewa.py

--- kodak Ewa.py
+++
@@ -1,9 +0,0 @@
- fullname = "Popoola Ewaade"
- ID = "HNG-02660"
- email = "popoolaaishat41@gmail.com"
- language = "Python"
- 
- def mytask():
-   print("Hello World, this is " + fullname + " with HNGi7 ID " + ID + " and email " + email + " using " + language + " for stage 2 task")
- 
- mytask()
