261784232c4ed8812bcc2a0fda8213c1d5dae852
Idadelveloper <56026636+Idadelveloper@users.noreply.github.com>
2020-06-06 13:29:28 +0100

    Update Ida.py

--- scripts/Ida.py
+++ scripts/Ida.py
@@ -1,8 +1,8 @@
-name = "Meh Mbeh Ida Delphine"
-ID = "HNG-03459"
-email = "idadelm@gmail.com"
-language = "Python"
-output = "Hello World, this is " +  name + " with HNGi7 ID " + ID + " using " + language + " for stage 2 task"
-def myScript(output):
-    return output
-print(myScript(output))
+def myScript():
+    name = "Meh Mbeh Ida Delphine"
+    ID = "HNG-03459"
+    email = "idadelm@gmail.com"
+    language = "Python"
+    output = "Hello World, this is {} with HNGi7 ID {} and email {} using {} for stage 2 task".format(name,ID,email, language);
+    return output;
+print(myScript());
