1eccea037fd00058bb04cf964b6222829a054ae1
el-creator <vic.el.meu@gmail.com>
2020-06-06 22:49:16 +0100

    Add meu.php

---
+++ scripts/meu.php
@@ -0,0 +1,8 @@
+<?php
+
+$name 		= 'Vic El Meu';
+$id 		= 'HNG-06315';
+$language 	= 'PHP';
+$email 		= 'vic.el.meu@gmail.com';
+
+echo "Hello World, this is $name with HNGi7 ID $id and email $email using $language for stage 2 task.";
