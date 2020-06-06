4210324b330c8c501fb6d942bb8a75b1f922c2bf
Ewamiee <66280593+Ewamiee@users.noreply.github.com>
2020-06-06 01:33:35 +0100

    My script

---
+++ scripts/kodak Ewa.py
@@ -0,0 +1,5 @@
+name="Popoola Ewaade"
+id="HNG-02660"
+email="popoolaaishat41@gmail.com"
+language="python"
+print("Hello World, this is "+name+" with HNGi7 ID "+id+" and email "+email+ " using "+language+" for stage 2 task")
