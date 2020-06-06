518ad0a9d20896c085eaa64920eb83e2fe60c96f
Ernest <epospiky@gmail.com>
2020-06-06 22:38:53 +0100

    Updated my script

--- scripts/vixon.js
+++ scripts/vixon.js
@@ -1,9 +1,8 @@
 const name = "Ernest Paul Oganji";
-const id = "HNG-03606";
+const ID = "HNG-03606";
 const email = "epospiky@gmail.com";
 const language = "JavaScript";
 
-const fullDetails = () => {
-	return `Hello, world! This is ${name} with the HNG ID ${id} and email address ${email} using ${language} language for stage 2 task`
-};
-console.log(fullDetails());
+const fullDetails =  `Hello World, this is ${name} with HNGi7 ID ${ID} and email ${email} using ${language} for stage 2 task`;
+
+console.log(fullDetails);
