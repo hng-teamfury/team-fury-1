1967ec945c72bcc5c48441ea82ccc31496a084b3
Henry <ec2-user@ip-172-31-2-113.eu-west-2.compute.internal>
2020-06-06 03:05:37 +0000

    Added script entry for Henry Ezedinma (FizzBuzz)

---
+++ scripts/FizzBuzz.py
@@ -0,0 +1,10 @@
+fullname = "Henry Ezedinma"
+hngid = "HNG-06293"
+language = "Python"
+email = "henry.ezedinma@outlook.com"
+
+def runscript(fullname,hngid,language,email):
+    return f"Hello World, this is {fullname} with HNGi7 ID {hngid} and email {email} using {language} for stage 2 task"
+
+
+print(runscript(fullname,hngid,language,email))
