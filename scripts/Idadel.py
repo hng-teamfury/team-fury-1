75be97cc5f202de6274c139eabb5e051d06009fc
Idadelveloper <56026636+Idadelveloper@users.noreply.github.com>
2020-06-06 10:45:00 +0100

    Update Idadel.py

--- scripts/Idadel.py
+++ scripts/Idadel.py
@@ -3,8 +3,9 @@ def myScript():
     ID = "HNG-03459"
     email = "idadelm@gmail.com"
     language = "Python"
-    output = "Hello World, this is {name} with HNGi7 ID {ID}. My email is {email} and I use the {language} programming language for stage 2 task
+    output = "Hello World, this is " +  name + " with HNGi7 ID " + ID + " using " + language + " for stage 2 task."
     
     return output
 
 print(myScript())
+
