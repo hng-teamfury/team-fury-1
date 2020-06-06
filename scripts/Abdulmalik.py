0829ad0854e379782dec866bd278ef4b873efe01
Muhammed Abdulmalik <49653491+amuhd@users.noreply.github.com>
2020-06-06 08:29:00 +0100

    add my script

---
+++ scripts/Abdulmalik.py
@@ -0,0 +1,10 @@
+name = "Muhammed Abdulmalik"
+id = "HNG-02602"
+email = "abd.muhd01@gmail.com"
+language = "Python"
+
+def script(name, id, email, language):
+    return f"Hello world, this is {name} with HNGi7 ID {id} and email {email} using {language} for stage 2 task"
+
+print(script(name, id, email, language))
+
