558530ff52050373da41cfabedf6c3dfce6faa17
Francisop <francisohis@gmail.com>
2020-06-06 13:17:07 +0100

    updated

--- scripts/francis.py
+++ scripts/francis.py
@@ -1,9 +1,7 @@
-name="Francis Onwukwe"
+
+name="francis onwukwe"
 id = "03944"
-language = "Python"
+email ='francisohis@gmail.com'
+language = "python"
 
-
-def script(name,id,email,language):
-    return f"Hello World, this is {name} with HNGi7 ID {id} using {language} for stage 2 task"
-  
-print(script(name,id,language))
+print(f'Hello World, this is {name} with HNGi7 ID {id} using {language} for stage 2 task')
