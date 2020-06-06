3df9fe3c159ce21456fbff17f1af740fc00ef828
Ameer Hamza <ameernormie@gmail.com>
2020-06-06 13:21:12 +0500

    feat: add script to print sentence for stage 1 task

---
+++ scripts/Ameer.js
@@ -0,0 +1,14 @@
+/**
+ * This script when run from terminal prints the sentence with given variables
+ * This script uses template literals of javascript.
+ * This script uses the Javascript to print the sentence
+ */
+
+const full_name = "Ameer Hamza";
+const HNG_ID = "HNG-03311";
+const email = "ameernormie@gmail.com";
+const programming_language = "javascript";
+
+console.log(
+  `Hello World, this is ${full_name} with HNGi7 ID ${HNG_ID} and email ${email} using ${programming_language} for stage 2 task`
+);
