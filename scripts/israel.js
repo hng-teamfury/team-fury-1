4aecdbae09dbc880e8587727b81b15106de35c9c
adeoyebrass <52764075+adeoyebrass@users.noreply.github.com>
2020-06-06 17:29:15 +0100

    Update israel.js

--- scripts/israel.js
+++ scripts/israel.js
@@ -3,6 +3,6 @@ Let HNGID="HNG-04195";
 Let email="adeoyefemi3@gmail.com";
 Let language="JavaScript";
 
-Const pass=`Hello world,this is ${fullname} with HNGi7 ID ${HNG-04195} and email ${email} using ${language} for stage 2 task`;
+Const pass=`Hello world,this is ${fullname} with HNGi7 ID ${HNGID} and email ${email} using ${language} for stage 2 task`;
 
 Console.log(pass);
