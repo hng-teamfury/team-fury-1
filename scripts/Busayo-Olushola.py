7974dbdcb634c46fdf9fc8369671c9288e87e28e
busgandy <39296111+busgandy@users.noreply.github.com>
2020-06-06 21:07:11 +0100

    Update Busayo-Olushola.py

--- scripts/Busayo-Olushola.py
+++ scripts/Busayo-Olushola.py
@@ -1,15 +1,5 @@
-def main():
-    print_details()
-
-def print_details():
-    full_name = ("Busayo Olushola")
-    HNGi7_ID = ("HNG-02411")
-    language = ("python")
-    task = ("stage 2 task")
-    e_mail = ("gideonbusayo@gmail.com")
-    print ("Hello World, this is", full_name, "with HNGi7 ID", str(HNGi7_ID), "and", str(e_mail),"using", language, "for", str(task))
-
-
-if __name__ == "__main__":
-    main()
-
+name = "Busayo Olushola"
+id = "HNG-02411"
+email = "gideonbusayo@gmail.com"
+language = "Python"
+print(f'Hello World, this is {name} with HNGi7 ID {id} and email {email} using {language} for stage 2 task')
