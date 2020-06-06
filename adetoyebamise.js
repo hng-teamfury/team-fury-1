a2883ec242b8d03f2c6e14b8b5cb3070ac801f74
Adetoyebamise <55400020+Adetoyebamise@users.noreply.github.com>
2020-06-05 20:38:37 -0700

    Update adetoyebamise.js

--- adetoyebamise.js
+++ adetoyebamise.js
@@ -6,6 +6,6 @@ const requirements ={
       language: "Javascript",
       email: "adetoyebamise@gmail.com"
     }
-    const statement = `Hello World, this is ${requirements.firstName} ${requirements.lastName} with HNGi7 ID ${requirements.HNGi7_ID} and email ${requirements.email} using ${requirements.language} for stage2 task`;
+    const statement = `Hello World, this is ${requirements.firstName} ${requirements.lastName} with HNGi7 ID ${requirements.HNGi7_ID} and email ${requirements.email} using ${requirements.language} for stage 2 task`;
 
     console.log(statement); 
