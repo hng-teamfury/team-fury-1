15f2b094488ddf2fc2654d10f3288811912d2849
Idadelveloper <idadelm@gmail.com>
2020-06-06 10:01:48 +0100

    Added my script

---
+++ scripts/Idadel.py
@@ -0,0 +1,10 @@
+def myscript():
+    Name = "Meh Mbeh Ida Delphine"
+    ID = "HNG-03459"
+    email = "idadelm@gmail.com"
+    language = "Python"
+
+
+    print(f"Hello World, this is {Name} with HNGi7 ID {ID} and email {email} using {language} for stage 2 task")
+
+myscript()
