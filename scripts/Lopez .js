3600c27e648ecd32827f8526d830954bb169e144
JoanneLopez <littleyetty@gmail.com>
2020-06-06 01:31:06 -0700

    Modified script

--- scripts/Lopez .js
+++ scripts/Lopez .js
@@ -1,11 +1,1 @@
-const sendMessage = () => {
-    const me = {
-   name: 'Lopez Yetunde',
-   id: 'HNG-00618',
-   email: 'littleyetty@gmail.com',
-   language: 'JavaScript'
-   }
-     const { name, id, email, language } = me;
-     return `Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`
-}
-console.log(sendMessage())
+console.log("Hello World, this is Lopez Yetunde with HNGi7 ID HNG-00618 and email littleyetty@gmail.com using Javascript for stage 2 task");
