846b2ec64890d73bd78372e53e0c01345191e705
MmonimUkpanah <mukpanah22@gmail.com>
2020-06-06 14:07:11 +0100

    I pray this works

---
+++ scripts/Mmonim-Ukpanah.js
@@ -0,0 +1,12 @@
+const fullName = "Mmonim Ukpanah"
+const id = "HNG-00919"
+const languageUsed= "Javascript"
+const emailAddress= "mukpanah22@gmail.com" 
+
+let helloWorld=()=>{
+console.log (`Hello World, this is ${fullName} with HNGi7 ID ${id} and email ${emailAddress} using ${languageUsed} for stage 2 taskgit`)
+}
+
+
+
+helloWorld();
