f3811fe3f981f60b291ed222267a471c113bbfe9
sawlew <sawlew@gmail.com>
2020-06-06 22:04:40 +0100

    sawlew.js

--- scripts/sawlew.js
+++ scripts/sawlew.js
@@ -1,1 +1,8 @@
-console.log("Hello World, this is Adeosun Soliu with HNGi7 ID HNG-06125 and email sawlew@gmail.com using Javascript for stage 2 task");
+
+    const fullName = "Adeosun Soliu kolade";
+    const hngId = "HNG-06125";
+    const email = "sawlew@gmail.com";
+    const language = "Javascript";
+
+const result = 'Hello World, this is ${fullName} with HNGi7 ID ${hngId} and email {email} using ${language} for stage 2 task';
+    console.log(result);
