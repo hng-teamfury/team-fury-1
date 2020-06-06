e1113fae0afdf2da795e9f9c6de83094998a474a
Muhammed Abdulmalik <49653491+amuhd@users.noreply.github.com>
2020-06-06 23:42:09 +0100

    Update amuhd.py

--- scripts/amuhd.py
+++ scripts/amuhd.py
@@ -1,1 +1,1 @@
-print ("Hello World, this is Muhammed Abdulmalik with HNGi7 ID HNG-02602 using Python for stage 2 task")
+print("Hello World, this is Muhammed Abdulmalik with HNGi7 ID HNG-02602 and email abd.muhd01@gmail.com using Python for stage 2 task")
