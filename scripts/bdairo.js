6e913ce1f6c994e954d5554481167d09e19f43d8
Bolu Dairo <dairobolu@gmail.com>
2020-06-07 12:04:07 -0500

    script updated

--- scripts/bdairo.js
+++ scripts/bdairo.js
@@ -1,3 +1,10 @@
 let name= "Boluwarin Dairo";
-let id = "INTERNSHIP ID: HNG-01402"
-console.log(" Hello World, This is " + name + " with " + id +" using javascript for stage 2 task" );
+let id = "INTERNSHIP ID: HNG-01402";
+let language="Javascript";
+let email = 'abisoyeoladayo@gmail.com';
+let output= " Hello World, This is " + name + " with " + id +" using " +language +" for stage 2 task";
+
+function display(){
+    return output;
+}
+console.log(output);
