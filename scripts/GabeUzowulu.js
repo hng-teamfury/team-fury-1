ae16f24c518a1b95bd7bc536a1fb297a9930ca9b
Gabriel Uzowulu <gabrieluzowulu@gmail.com>
2020-06-07 21:25:24 +0100

    I corrected my code
    Test didn't run successfully in the previous code but now it's better.
    PLs merge ASAP. Pls... Thanks

--- scripts/GabeUzowulu.js
+++ scripts/GabeUzowulu.js
@@ -1,6 +1,14 @@
-var name = 'Gabriel Uzowulu'
-var id = 'HNG-05095'
-var email = 'gabrieluzowulu@gmail.com'
-var language = 'javascript'
+function myHngInfo(){
 
-console.log('Hello World, this is ' + name + ' with HNGi7 ID ' + id + 'and email'+ email + ' using ' + language + ' for stage 2 task')
+      var id = 'HNG-05095';
+   var email = 'gabrieluzowulu@gmail.com';
+ var language = 'javascript';
+var fullname = 'Gabriel Uzowulu';
+
+
+  console.log('Hello World, this is '+ fullname+ ' with HNGi7 ID '+ id + ' and email ' + email +' using '+ language + ' for stage 2 task');
+
+
+}
+
+myHngInfo();
