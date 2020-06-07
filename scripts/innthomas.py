866064a2230dd46d228071f79ac6c2b26aeba422
INNOCENT THOMAS <innthomas00158@gmail.com>
2020-06-07 02:24:11 +0100

    errors corrected

--- scripts/innthomas.py
+++ scripts/innthomas.py
@@ -4,5 +4,5 @@ ID="HNG-00678"
 langage="Python"
 email="innthomas00158@gmail.com"
 def myData():
-    print ("Hello Word, this is {} with HNGi7 ID {} using {} for stage 2 task".format(fullName, ID, langage))
+    print ("Hello Word, this is {} with HNGi7 ID {}  and email {} using {} for stage 2 task".format(fullName, ID,email ,langage))
 myData()
