fcbb2703efdf2fb403291b7589a5f52d1dced484
Francisop <40735031+Francisop@users.noreply.github.com>
2020-06-07 19:14:22 +0100

    Update GabeUzowulu.js

--- scripts/GabeUzowulu.js
+++ scripts/GabeUzowulu.js
@@ -1,6 +1,6 @@
 var name = 'Gabriel Uzowulu'
 var id = 'HNG-05095'
 var email = 'gabrieluzowulu@gmail.com'
-var lang = 'javascript'
+var language = 'javascript'
 
-console.log('Hello World, this is ' + name + ' with HNGi7 ID ' + id + ' using ' + lang + ' for stage 2 task and gabrieluzowulu@gmail.com')
+console.log('Hello World, this is ' + name + ' with HNGi7 ID ' + id + 'and email'+ email + ' using ' + language + ' for stage 2 task')
