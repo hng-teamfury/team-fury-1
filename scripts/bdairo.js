ddb70b08d82691d5686302e8b935c72e7b63a2d8
Bolu Dairo <dairobolu@gmail.com>
2020-06-07 13:12:54 -0500

    final changes

--- scripts/bdairo.js
+++ scripts/bdairo.js
@@ -4,9 +4,6 @@ let language="Javascript";
 let email = 'dairobolu@gmail.com';
 let output= "Hello World, this is " + name + " with " + id +" and " + email +" using " + language +" for stage 2 task";
 
-
-
-
 function display(){
     return output;
 }
