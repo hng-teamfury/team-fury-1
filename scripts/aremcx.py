2ab96bede7831d83ad46ebc684ab7fc83c259368
aremcx84 <aremcx84@gmail.com>
2020-06-07 17:41:15 +0100

    aremcx.py

--- scripts/aremcx.py
+++ scripts/aremcx.py
@@ -1,7 +1,7 @@
-a = 'Hello World'
 fullName= "Abdul'wakil Jamiu"
 HNGi7_ID = 'HNG-04287'
 email = 'aremcx84@gmail.com'
-lang = 'python'
-myDetails = '{0} this is {1} with HNGi7 ID {2} and email {3} using {4} for stage 2'
-print(myDetails.format(a, fullName, HNGi7_ID, email, lang))
+language = 'Python'
+myDetails = 'Hello World, this is {0} with HNGi7 ID {1} and email {2} using {3} for stage 2 task'
+print(myDetails.format(fullName, HNGi7_ID, email, language))
+#print("Hello World, this is Abdul'wakil Jamiu with HNGi7 ID HNG-04287 and email aremcx84@gmail.com using Python for stage 2 task")
