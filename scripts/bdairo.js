e0c6eb028a7e97f802638f96089b64b2de44a5e6
Francisop <40735031+Francisop@users.noreply.github.com>
2020-06-07 19:27:37 +0100

    Update bdairo.js

--- scripts/bdairo.js
+++ scripts/bdairo.js
@@ -1,6 +1,6 @@
-let name= "Boluwarin Dairo";
-let id = "HNG-01402";
-let language="Javascript";
-let email = 'dairobolu@gmail.com';
-console.log("Hello World, this is "+ name +" with HNGi7 ID "+ id +" and email " + email +" using " + language + " for stage 2 task.");
+var name = "Boluwarin Dairo";
+var email = "dairobolu@gmail.com";
+var ID = "HNG-01402";
+var language = "JavaScript";
 
+console.log( "Hello world, " + "this is " + name + " with HNGi7 ID " + ID + " and email " + email + " using " + language  + " for stage 2 task";);
