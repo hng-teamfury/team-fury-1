445be3c66552ffcd79d1d40352c1115e2d2909e3
Joshua Etuk <contactetuk@gmail.com>
2020-06-07 16:07:54 +0100

    my task 2 script

---
+++ cujjo.js
@@ -0,0 +1,8 @@
+//This is my script for HNG task 2
+const name = 'Joshua Etuk';
+const id = 'HNG-06385';
+const language = 'JavaScript';
+const email = 'contactetuk@gmail.com';
+
+const myString = `Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`;
+console.log(myString);
