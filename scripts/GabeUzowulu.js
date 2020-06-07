92f695f6329ef511548afd0f25f185a2efe3d86b
Gabriel Uzowulu <gabrieluzowulu@gmail.com>
2020-06-07 17:02:17 +0100

    Create GabeUzowulu.js
    Added my script to the repo

---
+++ scripts/GabeUzowulu.js
@@ -0,0 +1,7 @@
+var name = 'Gabriel Uzowulu'
+var id = 'HNG-05095'
+var email = 'gabrieluzowulu@gmail.com
+var lang = 'javascript'
+
+console.log('Hello World, this is ' + name + ' with HNGi7 ID ' + id + ' using ' + lang + ' for stage 2 task and gabrieluzowulu@gmail.com')
+© 2020 GitHub, Inc.
