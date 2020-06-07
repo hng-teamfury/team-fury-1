2882a67182e1404ae139202c4c23bec08af60435
Gabriel Uzowulu <gabrieluzowulu@gmail.com>
2020-06-07 18:27:29 +0100

    Update GabeUzowulu.js

--- scripts/GabeUzowulu.js
+++ scripts/GabeUzowulu.js
@@ -1,6 +1,6 @@
 var name = 'Gabriel Uzowulu'
 var id = 'HNG-05095'
-var email = 'gabrieluzowulu@gmail.com
+var email = 'gabrieluzowulu@gmail.com'
 var lang = 'javascript'
 
 console.log('Hello World, this is ' + name + ' with HNGi7 ID ' + id + ' using ' + lang + ' for stage 2 task and gabrieluzowulu@gmail.com')
