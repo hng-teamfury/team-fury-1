3875cc8c61ebdcf5a76ba717b81528a71dfe43ab
Francisop <40735031+Francisop@users.noreply.github.com>
2020-06-07 19:35:51 +0100

    Update aremcx.py

--- scripts/aremcx.py
+++ scripts/aremcx.py
@@ -1,7 +1,7 @@
-fullName= "Abdul'wakil Jamiu"
-HNGi7_ID = 'HNG-04287'
-email = 'aremcx84@gmail.com'
-language = 'Python'
-myDetails = 'Hello World, this is {0} with HNGi7 ID {1} and email {2} using {3} for stage 2 task'
-print(myDetails.format(fullName, HNGi7_ID, email, language))
-#print("Hello World, this is Abdul'wakil Jamiu with HNGi7 ID HNG-04287 and email aremcx84@gmail.com using Python for stage 2 task")
+name='Abdulwakil Jamiu'
+email='Joshkid610@gmail.com'
+hng_id='HNG-04287'
+language='PYTHON'
+
+print(f"Hello World, this is {name} with HNGi7 ID {hng_id} and email {email} using {language} for stage 2 task")
+
