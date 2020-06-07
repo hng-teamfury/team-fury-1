06813a869c1dcca01c1fea3d58661529d59e89df
System Administrator <eddymav247@gmail.com>
2020-06-07 06:49:36 +0100

    my script in js

---
+++ scripts/maverick.js
@@ -0,0 +1,7 @@
+function myDetails(name, id ,email,language, stage) {
+   return `Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage ${stage} task`
+ };
+ myDetails('Edidiong Ekpo', 'HNG-02187','eddymav247@gmail.com', 'javaScript', '1');
+
+
+ console.log(myDetails('Edidiong Ekpo', 'HNG-02187','eddymav247@gmail.com', 'javaScript', '1'));
