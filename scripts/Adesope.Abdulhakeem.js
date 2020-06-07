d77860079518e38b9272474dc4353003015be134
Adesope Abdulhakeem <adesopebdulhakeem1@gmail.com>
2020-06-07 02:06:14 +0100

    adesope script added

---
+++ scripts/Adesope.Abdulhakeem.js
@@ -0,0 +1,8 @@
+let name = "Adesope Abdulhakeem";
+let HNGid = "HNG-05798";
+let Langauage = "JavaScript";
+let Email = "adesopeabdulhakeem1@gmail.com";
+
+
+
+console.log("Hello World, this is " + name + " with HNGi7 ID " + HNGid +  " using " + Langauage + " for stage 2 task");
