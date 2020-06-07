33e96ab34f2731b710ae39f44b1576c12de9889c
Bolu Dairo <dairobolu@gmail.com>
2020-06-07 13:22:44 -0500

    This should work

--- scripts/bdairo.js
+++ scripts/bdairo.js
@@ -2,9 +2,5 @@ let name= "Boluwarin Dairo";
 let id = "HNG-01402";
 let language="Javascript";
 let email = 'dairobolu@gmail.com';
-let output= "Hello World, this is " + name + " with " + id +" and " + email +" using " + language +" for stage 2 task";
+console.log("Hello World, this is "+ name +" with HNGi7 ID "+ id +" and email " + email +" using " + language + " for stage 2 task.");
 
-function display(){
-    return output;
-}
-console.log(output);
