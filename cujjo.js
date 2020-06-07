4206b203925dc96898b28f49915916f90772e5cb
Francisop <40735031+Francisop@users.noreply.github.com>
2020-06-07 19:23:13 +0100

    Update cujjo.js

--- cujjo.js
+++ cujjo.js
@@ -1,8 +1,6 @@
-//This is my script for HNG task 2
-const name = 'Joshua Etuk';
-const id = 'HNG-06385';
-const language = 'JavaScript';
-const email = 'contactetuk@gmail.com';
+var name = 'Joshua Etuk'
+var id = 'HNG-06385'
+var email = 'gabrieluzowulu@gmail.com'
+var language = 'javascript'
 
-const myString = `Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`;
-console.log(myString);
+console.log('Hello World, this is ' + name + ' with HNGi7 ID ' + id + 'and email'+ email + ' using ' + language + ' for stage 2 task')
