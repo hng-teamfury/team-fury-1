871c91210107acc42a33143fbbe708d203177f4b
Gabriel Uzowulu <gabrieluzowulu@gmail.com>
2020-06-07 18:36:39 +0100

    Updated

--- scripts/GabeUzowulu.js
+++ scripts/GabeUzowulu.js
@@ -4,4 +4,3 @@ var email = 'gabrieluzowulu@gmail.com'
 var lang = 'javascript'
 
 console.log('Hello World, this is ' + name + ' with HNGi7 ID ' + id + ' using ' + lang + ' for stage 2 task and gabrieluzowulu@gmail.com')
-© 2020 GitHub, Inc.
