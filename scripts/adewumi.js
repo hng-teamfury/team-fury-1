8d4dc4545fece7147cef60cb3b5d008ab879f411
Adelakin Adewumi <61943042+Adelakin-Adewumi@users.noreply.github.com>
2020-06-07 03:01:52 -1200

    Create adewumi.js

---
+++ scripts/adewumi.js
@@ -0,0 +1,5 @@
+let fullname = "Adelain Adewumi";
+        let language = "Javascript";
+        let id = "HNG-04266";
+        let email = "adelakinadewumi@gmail.com";
+        console.log("Hello World, this is" + " " + fullname + " " + "with HNGi7 ID" + " " + id + " and email " + email + " " + "using" + " " + language + " " + "for stage 2 task ")
