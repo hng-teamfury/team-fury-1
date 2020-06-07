b77c0532b3632b80a6c9c3f4c3f6c14f10d1774b
Crestward <65926941+Crestward@users.noreply.github.com>
2020-06-07 12:09:01 +0100

    Update Dimade.py

--- scripts/Dimade.py
+++ scripts/Dimade.py
@@ -3,7 +3,4 @@ HNG_ID = "HNG-02518"
 Email = "oladimeji759@gmail.com"
 Language = "Python"
 
-def output():
-    print (f"Hello world, this is {full_name} with HNDi7 ID {HNG_ID} using {Language} for stage 2 task.")
-
-output()
+print ("Hello world, this is " + full_name + " with HNDi7 ID " + HNG_ID + " using " + Language + " for stage 2 task.")
