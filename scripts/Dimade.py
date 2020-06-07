fe3a6fa2b78cf8269d9c3d051b0c58d8265e7a09
Crestward <65926941+Crestward@users.noreply.github.com>
2020-06-07 12:14:44 +0100

    Delete Dimade.py

--- scripts/Dimade.py
+++
@@ -1,6 +0,0 @@
-full_name = "Oladimeji Olamide Adeyemi"
-HNG_ID = "HNG-02518"
-Email = "oladimeji759@gmail.com"
-Language = "Python"
-
-print ("Hello world, this is " + full_name + " with HNDi7 ID " + HNG_ID + " using " + Language + " for stage 2 task.")
